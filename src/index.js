b5e5640c01b5521c48d6c59f5174bdbabf78de8a
qudcks8084 <qudcks8084@naver.com>
2024-04-06 19:51:09 +0900

    [MOD] 전체 폴더 구조 변경

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CodeHome from './CodeHome/CodeHome'
+import CodeHome from './CodeCommunity/CodeHome/CodeHome'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
