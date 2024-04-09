61e34f1e04fde1a47d5856a84bd37a33b60bd803
qudcks8084 <qudcks8084@naver.com>
2024-04-09 17:36:59 +0900

    [ADD] Route 사용을 위해 App.js 추가 및 Index.js 수정

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MyProfile from './MyPage/MyProfile/MyProfile';
+
+function App(){
+    return(
+        <MyProfile />
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MyProfile from './MyPage/MyProfile/MyProfile';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyProfile />
+    <App />
   </React.StrictMode>
 );
 
