300ea7eb0ed5815b6dd48093b4ef5188fb2489c0
qudcks8084 <qudcks8084@naver.com>
2024-04-10 14:48:33 +0900

    [ADD] CODE 검색 결과 출력용 데모 데이터 및 출력 컴포넌트 생성

---
+++ src/CodeCommunity/CodeDetail/CodeDetail.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './CodeDetail.css';
+
+const DemoData = [
+    {
+        "id": 1,
+        "name": "Byeongchan",
+        "referencedCount": 2,
+        "verified": true,
+        "language": "JAVA"
+      },
+      {
+        "id": 2,
+        "name": "Rangjin",
+        "referencedCount": 2,
+        "verified": true,
+        "language": "JAVA"
+      },
+      {
+        "id": 3,
+        "name": "HyunWoo",
+        "referencedCount": 2,
+        "verified": true,
+        "language": "JAVA"
+      },
+      {
+        "id": 123,
+        "name": "MonKee",
+        "referencedCount": 2,
+        "verified": true,
+        "language": "JAVA"
+      }
+]
+
+function CodeDetail(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default CodeDetail;
