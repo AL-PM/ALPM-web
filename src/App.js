c8625df08cd4f73795c26c8cd0e1686761745ad7
qudcks8084 <qudcks8084@naver.com>
2024-04-10 18:06:03 +0900

    [TEST]

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import CodeHome from './CodeCommunity/CodeHome/CodeHome';
 function App(){
     return(
         <CodeHome />
-
-        /*
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<StudyHome />} />
-                <Route path="/mypage" element={<MyProfile />} />
-                <Route path="/code" element={<CodeHome />} />
-                <Route path="/codegroup" element={<CodeGroupHome />} />
-            </Routes>
-        </BrowserRouter>
-        */
     )
 }
 
