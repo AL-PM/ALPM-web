02a3b0d012a7d0dcf51c3384d8f6641beeabb323
qudcks8084 <qudcks8084@naver.com>
2024-04-10 16:12:54 +0900

    [MOD] App.js Route 경로 추가 및 Package.json React-Router-Dom 버전 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import MyProfile from './MyPage/MyProfile/MyProfile';
+import StudyHome from './Study/StudyHome/StudyHome';
+import CodeHome from './CodeCommunity/CodeHome/CodeHome';
+import CodeGroupHome from './CodeGroupCommunity/CodeGroupHome/CodeGroupHome';
+
 
 function App(){
     return(
-        <MyProfile />
+        <Router>
+            <Switch>
+                <Route path="/"><StudyHome /></Route>
+                <Route path="/mypage"><MyProfile /></Route>
+                <Route path="/codecommunity"><CodeHome /></Route>
+                <Route path="/codegroupcommunity"><CodeGroupHome /></Route>
+            </Switch>
+        </Router>
     )
 }
 
