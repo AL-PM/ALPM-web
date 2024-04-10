b0ed2f0cad98a0ccb7703e29b082761cdaa2ba9a
qudcks8084 <qudcks8084@naver.com>
2024-04-10 17:49:54 +0900

    [DEL] React-Router-Dom 관련 내용 삭제

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MyProfile from './MyPage/MyProfile/MyProfile';
-import StudyHome from './Study/StudyHome/StudyHome';
 import CodeHome from './CodeCommunity/CodeHome/CodeHome';
-import CodeGroupHome from './CodeGroupCommunity/CodeGroupHome/CodeGroupHome';
-
 
 function App(){
     return(
-        <BrowserRouter>
-            <Switch>
-                <Route path="/home"><StudyHome /></Route>
-                <Route path="/mypage"><MyProfile /></Route>
-                <Route path="/code"><CodeHome /></Route> 
-                <Route path="/codegroup"><CodeGroupHome /></Route> 
-            </Switch>
-        </BrowserRouter>
+        <CodeHome />
 
         /*
         <BrowserRouter>
