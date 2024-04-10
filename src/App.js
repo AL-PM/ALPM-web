c536f8dc0f0461250b4923d464a0128c4ba02ec5
qudcks8084 <qudcks8084@naver.com>
2024-04-10 18:40:15 +0900

    [MOD] App.js MainMenuBar 관련 React-Router-Dom 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import CodeHome from './CodeCommunity/CodeHome/CodeHome';
+import StudyHome from "./Study/StudyHome/StudyHome";
+import MyProfile from "./MyPage/MyProfile/MyProfile";
+import CodeGroupHome from "./CodeGroupCommunity/CodeGroupHome/CodeGroupHome";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App(){
     return(
-        <CodeHome />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<StudyHome />} />
+                <Route path="/mypage" element={<MyProfile />} />
+                <Route path="/code" element={<CodeHome />} />
+                <Route path="/codegroup" element={<CodeGroupHome />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- src/Etc/MainMenuBar/MainMenuBar.jsx
+++ src/Etc/MainMenuBar/MainMenuBar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import './MainMenuBar.css';
+import {useNavigate} from 'react-router-dom';
 
 function MainMenuBar({page}){
-    console.log(page)
+    const navigator = useNavigate();
     return(
         <div id="MainMenubar">
             <span id="MainMenuBarLogo">A.L.P.M</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="Study" ? "bold" : "normal", color:page ==="Study" ? "#5C4EFF" : "Black"}} onClick={()=>console.log('/')} >STUDY HOME</span>
+            <span style={{fontWeight:page ==="Study" ? "bold" : "normal", color:page ==="Study" ? "#5C4EFF" : "Black"}} onClick={()=>navigator('/')} >STUDY HOME</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="Code" ? "bold" : "normal", color:page ==="Code" ? "#FF6B00" : "Black"}} onClick={()=>console.log('/code')} >CODE COMMUNITY</span>
+            <span style={{fontWeight:page ==="Code" ? "bold" : "normal", color:page ==="Code" ? "#FF6B00" : "Black"}} onClick={()=>navigator('/code')} >CODE COMMUNITY</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="CodeGroup" ? "bold" : "normal", color:page ==="CodeGroup" ? "#009418" : "Black"}} onClick={()=>console.log('/codegroup')}>CODE GROUP COMMUNITY</span>
+            <span style={{fontWeight:page ==="CodeGroup" ? "bold" : "normal", color:page ==="CodeGroup" ? "#009418" : "Black"}} onClick={()=>navigator('/codegroup')}>CODE GROUP COMMUNITY</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="MyPage" ? "bold" : "normal", color:page ==="MyPage" ? "#EF4949" : "Black"}} onClick={()=>console.log('/mypage')}>MY PAGE</span>
+            <span style={{fontWeight:page ==="MyPage" ? "bold" : "normal", color:page ==="MyPage" ? "#EF4949" : "Black"}} onClick={()=>navigator('/mypage')}>MY PAGE</span>
         </div>
     )
 }
