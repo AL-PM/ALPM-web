076c8a90bec040c7dc7f39cfd953904f8b3ea762
qudcks8084 <qudcks8084@naver.com>
2024-04-10 17:46:49 +0900

    [MOD] 버그 수정을 위한 React-Router-Dom 버전 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MyProfile from './MyPage/MyProfile/MyProfile';
 import StudyHome from './Study/StudyHome/StudyHome';
 import CodeHome from './CodeCommunity/CodeHome/CodeHome';
@@ -9,6 +9,16 @@ import CodeGroupHome from './CodeGroupCommunity/CodeGroupHome/CodeGroupHome';
 function App(){
     return(
         <BrowserRouter>
+            <Switch>
+                <Route path="/home"><StudyHome /></Route>
+                <Route path="/mypage"><MyProfile /></Route>
+                <Route path="/code"><CodeHome /></Route> 
+                <Route path="/codegroup"><CodeGroupHome /></Route> 
+            </Switch>
+        </BrowserRouter>
+
+        /*
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<StudyHome />} />
                 <Route path="/mypage" element={<MyProfile />} />
@@ -16,6 +26,7 @@ function App(){
                 <Route path="/codegroup" element={<CodeGroupHome />} />
             </Routes>
         </BrowserRouter>
+        */
     )
 }
 
--- src/Etc/MainMenuBar/MainMenuBar.jsx
+++ src/Etc/MainMenuBar/MainMenuBar.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
 import './MainMenuBar.css';
 
 function MainMenuBar({page}){
-    const navigate = useNavigate();
     console.log(page)
     return(
         <div id="MainMenubar">
             <span id="MainMenuBarLogo">A.L.P.M</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="Study" ? "bold" : "normal", color:page ==="Study" ? "#5C4EFF" : "Black"}} onClick={()=>navigate('/')} >STUDY HOME</span>
+            <span style={{fontWeight:page ==="Study" ? "bold" : "normal", color:page ==="Study" ? "#5C4EFF" : "Black"}} onClick={()=>console.log('/')} >STUDY HOME</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="Code" ? "bold" : "normal", color:page ==="Code" ? "#FF6B00" : "Black"}} onClick={()=>navigate('/code')} >CODE COMMUNITY</span>
+            <span style={{fontWeight:page ==="Code" ? "bold" : "normal", color:page ==="Code" ? "#FF6B00" : "Black"}} onClick={()=>console.log('/code')} >CODE COMMUNITY</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="CodeGroup" ? "bold" : "normal", color:page ==="CodeGroup" ? "#009418" : "Black"}} onClick={()=>navigate('/codegroup')}>CODE GROUP COMMUNITY</span>
+            <span style={{fontWeight:page ==="CodeGroup" ? "bold" : "normal", color:page ==="CodeGroup" ? "#009418" : "Black"}} onClick={()=>console.log('/codegroup')}>CODE GROUP COMMUNITY</span>
             <span>|</span>
-            <span style={{fontWeight:page ==="MyPage" ? "bold" : "normal", color:page ==="MyPage" ? "#EF4949" : "Black"}} onClick={()=>navigate('/mypage')}>MY PAGE</span>
+            <span style={{fontWeight:page ==="MyPage" ? "bold" : "normal", color:page ==="MyPage" ? "#EF4949" : "Black"}} onClick={()=>console.log('/mypage')}>MY PAGE</span>
         </div>
     )
 }
