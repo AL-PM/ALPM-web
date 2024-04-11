5a848d79082d03044f91a5f549c9135d19d70ea2
qudcks8084 <qudcks8084@naver.com>
2024-04-11 14:47:50 +0900

    [MOD] CODE_GROUP 기존 사이트별 메뉴바에서 공통 메뉴바로 수정

--- src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
+++ src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
@@ -1,34 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CodeGroupHome.css";
 import CodeGroupSearchIcon from "./img/SearchIcon.png";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
-
-
-function CodeGroupHomeSearchBar(props){
-    return(
-        <div id="CodeGroupHomeSearchBar">
-            <span id = "CodeGroupSettingKeyword" >학습 언어</span>
-            <span>|</span>
-            <span id = "CodeGroupLanguageSetting1" >PYTHON</span>
-            <span id = "CodeGroupLanguageSetting1" >JAVA</span>
-            <span id = "CodeGroupLanguageSetting1" >C++</span>
-            <span></span>
-            <span id = "CodeGroupSettingKeyword" >그룹 제작자</span>
-            <span>|</span>
-            <span id = "CodeGroupType1" >ALL</span>
-            <span id = "CodeGroupType2" >REFERENCE</span>
-            <span id = "CodeGroupType3" >USER_MADE</span>
-            <span></span>
-            <span id = "CodeGroupSettingKeyword" >검색어</span>
-            <span>|</span>
-            <input id = "CodeGroupHomeSearchInput" type="text" name="CodeHomeSearchKeyword" placeholder="키워드를 입력해주세요"></input>
-            <img id = "CodeGroupHomeSearchButton" src={CodeGroupSearchIcon} alt="CodeGroupSearchIcon"></img>
-        </div>
-    )
-}
+import CommunitySearchBar from "../../Etc/CommunitySearchBar/CommunitySearchBar.jsx";
 
 function CodeGroupHomebody(){
-
     return(
         <div id="CodeGroupHomeBody">
         </div>
@@ -37,11 +13,13 @@ function CodeGroupHomebody(){
 
 
 function CodeGrupHome(){
-
+    const [language, setlanguage] = useState("PYTHON");
+    const [reference, setreference] = useState("ALL");
+    const [searchkeyword, setkeword] = useState("");
     return(
         <div id = "CodeGroupHome">
             <MainMenuBar page={"CodeGroup"} />
-            <CodeGroupHomeSearchBar />
+            <CommunitySearchBar secondTag={"제작자 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
             <CodeGroupHomebody />
         </div>
        
