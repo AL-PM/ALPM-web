9adc709527473a73704f7743ff248e92cee6cd3a
qudcks8084 <qudcks8084@naver.com>
2024-04-11 17:30:57 +0900

    [MOD] CODE_GROUP 코드 그룹 상세페이지 팔로우 버튼 및 Body Props 추가

--- src/CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail.jsx
+++ src/CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail.jsx
@@ -3,6 +3,13 @@ import {useLocation} from "react-router-dom"
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
 import CodeSearchResult from "../../Etc/CodeSearchResult/CodeSearchResult.jsx";
 import CodeGroupDetailInfo from "../../Etc/CodeGroupDetailInfo/CodeGroupDetailInfo.jsx";
+import './CodeGroupDetail.css';
+
+function CodeGroupFollowBtn(){
+    return(
+        <button id="CodeGroupFollowBtn">코드 그룹 팔로우하기</button>
+    )
+}
 
 function CodeGroupDetail(){
     const {state} = useLocation();
@@ -52,6 +59,29 @@ function CodeGroupDetail(){
                     "verified": null,
                     "language": "Python",
                     "owner" : "MoonKee"
+                },
+                {
+                    "id": 5,
+                    "name": "버블 정렬",
+                    "referencedCount": 4,
+                    "verified": null,
+                    "language": "Python",
+                    "owner" : "MoonKee"
+                },
+                {
+                    "id": 6,
+                    "name": "버블 정렬",
+                    "referencedCount": 4,
+                    "verified": null,
+                    "language": "Python",
+                    "owner" : "MoonKee"
+                },{
+                    "id": 7,
+                    "name": "버블 정렬",
+                    "referencedCount": 4,
+                    "verified": null,
+                    "language": "Python",
+                    "owner" : "MoonKee"
                 }
             ] 
         };
@@ -60,7 +90,8 @@ function CodeGroupDetail(){
         <div id="CodeGroupDetail">
             <MainMenuBar page={"CodeGroup"} />
             <CodeGroupDetailInfo language={exampleData.language} verified={exampleData.verified} owner={exampleData.owner.name} name={exampleData.name} numOfAlgorithm={exampleData.algorithms.length}/>
-            <CodeSearchResult searchData={exampleData.algorithms}/>
+            <CodeSearchResult searchData={exampleData.algorithms} bodyHeight={"55vh"}/>
+            <CodeGroupFollowBtn />
         </div>
     )
 }
