8e0ed862b02e56af7d41aade75590ee6f7a7341b
qudcks8084 <qudcks8084@naver.com>
2024-04-11 16:00:07 +0900

    [ADD] CODE_GROUP 코드 그룹 상세 조회 사이트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CodeHome from './CodeCommunity/CodeHome/CodeHome';
 import StudyHome from "./Study/StudyHome/StudyHome";
 import MyProfile from "./MyPage/MyProfile/MyProfile";
 import CodeGroupHome from "./CodeGroupCommunity/CodeGroupHome/CodeGroupHome";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CodeGroupDetail from "./CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail";
+
 
 function App(){
     return(
@@ -12,7 +14,8 @@ function App(){
                 <Route path="/" element={<StudyHome />} />
                 <Route path="/mypage/myprofile" element={<MyProfile />} />
                 <Route path="/code" element={<CodeHome />} />
-                <Route path="/codegroup" element={<CodeGroupHome />} />
+                <Route path="/codegroup/" element={<CodeGroupHome />} />
+                <Route path="/codegroup/Detail" element={<CodeGroupDetail />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
+
+function CodeGroupDetail({codeGroupId}){
+    return(
+        <div>
+            <MainMenuBar page={"CodeGroup"} />
+        </div>
+    )
+}
+
+export default CodeGroupDetail;
