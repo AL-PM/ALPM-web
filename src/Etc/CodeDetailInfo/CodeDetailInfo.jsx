dd1fbd0ca099670d77a0c0c43976fc6a23167983
qudcks8084 <qudcks8084@naver.com>
2024-04-11 18:07:55 +0900

    [ADD] ETC 코드 상세 페이지 정보 요약 바 추가

---
+++ src/Etc/CodeDetailInfo/CodeDetailInfo.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './CodeDetailInfo.css';
+
+function CodeDetailInfo({verified, language, owner, name}){
+    function verifiedInvertor(verified){
+        if(verified === null)
+            return "ALL";
+        if(verified === true)
+            return "REFERENCE";
+        if(verified === false)
+            return "USER_MADE";
+    }
+    return(
+        <div id="CodeDetailInfo">
+                <span>학습 언어</span>
+                <span>|</span>
+                <span>{language}</span>
+                <span></span>
+                <span>코드 유형</span>
+                <span>|</span>
+                <span>{verifiedInvertor(verified)}</span>
+                <span></span>
+                <span>작성자</span>
+                <span>|</span>
+                <span>{owner}</span>
+                <span></span>
+                <span>제목</span>
+                <span>|</span>
+                <span>{name}</span>
+            </div>
+    )
+}
+
+export default CodeDetailInfo;
