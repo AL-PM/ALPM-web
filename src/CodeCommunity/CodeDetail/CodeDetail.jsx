6953d383674e9005b29e0411afe97c8be31c648f
qudcks8084 <qudcks8084@naver.com>
2024-04-11 17:58:14 +0900

    [MOD] CODE 상세 페이지 더미데이터 구현 및 버튼 추가

--- src/CodeCommunity/CodeDetail/CodeDetail.jsx
+++ src/CodeCommunity/CodeDetail/CodeDetail.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import {useLocation} from "react-router-dom"
-import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
+import './CodeDetail.css';
+import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx";
+
+function CodeFollowBtn(){
+    return(
+        <button id="CodeFollowBtn">코드 그룹 팔로우하기</button>
+    )
+}
 
 function CodeDetail(){
+    const {state} = useLocation();
+    let exampleData = {
+        "id": {state},
+        "name": "TestCode", 
+        "referencedCount": 3,
+        "verified": true,
+        "language": "JAVA",
+        "content": "Test Code Ex",
+        "description": "Test description Ex",
+        "owner": {
+          "id": 20192830, 
+          "name": "Soongsil Univ",
+          "profile": "종강시켜주세요 제발 ㅠㅠ"
+        }
+      }
     return(
-        <div>
+        <div id="CodeDetail">
             <MainMenuBar page={"Code"} />
+            <CodeFollowBtn />
         </div>
     )
 
