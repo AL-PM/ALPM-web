bfa1835d4b1d225e068f2e4c94f271103d480be0
qudcks8084 <qudcks8084@naver.com>
2024-04-11 15:36:08 +0900

    [MOD] CODE_GROUP 더미 데이터 추가 및 검색 결과 출력 최대 길이 설정

--- src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
+++ src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
@@ -2,24 +2,84 @@ import React, { useState } from "react";
 import "./CodeGroupHome.css";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
 import CommunitySearchBar from "../../Etc/CommunitySearchBar/CommunitySearchBar.jsx";
-
-function CodeGroupHomebody(){
-    return(
-        <div id="CodeGroupHomeBody">
-        </div>
-    )
-}
+import CodeGroupSearchResult from "../../Etc/CodeGroupSearchResult/CodeGroupSearchResult.jsx";
 
 
 function CodeGrupHome(){
     const [language, setlanguage] = useState("PYTHON");
     const [reference, setreference] = useState("ALL");
     const [searchkeyword, setkeword] = useState("");
+    const searchData = [
+        {
+            "id": 1,
+            "name": "다양한 정렬 알고리즘",
+            "referencedCount": 2,
+            "verified": true,
+            "visible": true,
+            "language": "Python",
+            "owner" : "Byeongchan"
+        },
+        {
+            "id": 2,
+            "name": "스택 관련 자료 구조",
+            "referencedCount": 2,
+            "verified": true,
+            "visible": true,
+            "language": "Python",
+            "owner" : "Rangjin"
+        },
+        {
+            "id": 3,
+            "name": "Knapsack",
+            "referencedCount": 4,
+            "verified": true,
+            "visible": true,
+            "language": "Python",
+            "owner" : "HyeonWoo"
+        },
+        {
+            "id": 4,
+            "name": "n-queen problem",
+            "referencedCount": 4,
+            "verified": null,
+            "visible": true,
+            "language": "Python",
+            "owner" : "MoonKee"
+        },
+        {
+            "id": 5,
+            "name": "Linked-List",
+            "referencedCount": 4,
+            "verified": null,
+            "visible": true,
+            "language": "Python",
+            "owner" : "MoonKee"
+        },
+        {
+            "id": 6,
+            "name": "Binary Search",
+            "referencedCount": 4,
+            "verified": false,
+            "visible": true,
+            "language": "Python",
+            "owner" : "MoonKee"
+        },
+        {
+            "id": 7,
+            "name": "Tree Search",
+            "referencedCount": 4,
+            "verified": false,
+            "visible": true,
+            "language": "Python",
+            "owner" : "MoonKee"
+        },
+    ]
+
     return(
         <div id = "CodeGroupHome">
             <MainMenuBar page={"CodeGroup"} />
             <CommunitySearchBar secondTag={"제작자 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
-            <CodeGroupHomebody />
+            <CodeGroupSearchResult searchData={searchData}/>
         </div>
        
     )
