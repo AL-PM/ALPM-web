02ac72c94a8c9b4d9d0c5b59c976858d934348eb
qudcks8084 <qudcks8084@naver.com>
2024-04-11 14:51:58 +0900

    [MOD] ETC 커뮤니티 검색바 사이트별 색상 자동 변경 함수 추가

--- src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
+++ src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
@@ -2,23 +2,26 @@ import React from "react";
 import CodeSearchIcon from "./img/SearchIcon.png";
 import './CommunitySearchBar.css';
 
-function CommunitySearchBar({ secondTag, language, setlanguage, reference, setreference, setkeword, searchkeyword}){
+function CommunitySearchBar({secondTag, language, setlanguage, reference, setreference, setkeword, searchkeyword}){
     function searchButton(){
         console.log({secondTag, language, reference, searchkeyword});
+    }
+    function colorInvertor(secondTag){
+        return secondTag === "코드 유형" ? "#FF6B00" : "#009418";
     }
     return(
         <div id="CommunitySearchBar">
             <span id = "SearchBarSetting" >학습 언어</span>
             <span>|</span>
-            <span onClick={()=> setlanguage("PYTHON")} style={{fontWeight:language==="PYTHON" ? "bold" : "normal", color:language==="PYTHON" ? "#FF6B00" : "black"}} >PYTHON </span>
-            <span onClick={()=> setlanguage("JAVA")} style={{fontWeight:language==="JAVA" ? "bold" : "normal", color:language==="JAVA" ? "#FF6B00" : "black"}} >JAVA</span>
-            <span onClick={()=> setlanguage("C++")} style={{fontWeight:language==="C++" ? "bold" : "normal", color:language==="C++" ? "#FF6B00" : "black"}} >C++</span>
+            <span onClick={()=> setlanguage("PYTHON")} style={{fontWeight:language==="PYTHON" ? "bold" : "normal", color:language==="PYTHON" ? colorInvertor(secondTag) : "black"}} >PYTHON </span>
+            <span onClick={()=> setlanguage("JAVA")} style={{fontWeight:language==="JAVA" ? "bold" : "normal", color:language==="JAVA" ? colorInvertor(secondTag) : "black"}} >JAVA</span>
+            <span onClick={()=> setlanguage("C++")} style={{fontWeight:language==="C++" ? "bold" : "normal", color:language==="C++" ? colorInvertor(secondTag) : "black"}} >C++</span>
             <span></span>
             <span id = "SearchBarSetting" >{secondTag}</span>
             <span>|</span>
-            <span onClick={()=> setreference("ALL")} style={{fontWeight:reference==="ALL" ? "bold" : "normal", color:reference==="ALL" ? "#FF6B00" : "black"}} >ALL</span>
-            <span onClick={()=> setreference("REFERENCE")} style={{fontWeight:reference==="REFERENCE" ? "bold" : "normal", color:reference==="REFERENCE" ? "#FF6B00" : "black"}} >REFERENCE</span>
-            <span onClick={()=> setreference("USER_MADE")} style={{fontWeight:reference==="USER_MADE" ? "bold" : "normal", color:reference==="USER_MADE" ? "#FF6B00" : "black"}} >USER_MADE</span>
+            <span onClick={()=> setreference("ALL")} style={{fontWeight:reference==="ALL" ? "bold" : "normal", color:reference==="ALL" ? colorInvertor(secondTag) : "black"}} >ALL</span>
+            <span onClick={()=> setreference("REFERENCE")} style={{fontWeight:reference==="REFERENCE" ? "bold" : "normal", color:reference==="REFERENCE" ? colorInvertor(secondTag) : "black"}} >REFERENCE</span>
+            <span onClick={()=> setreference("USER_MADE")} style={{fontWeight:reference==="USER_MADE" ? "bold" : "normal", color:reference==="USER_MADE" ? colorInvertor(secondTag) : "black"}} >USER_MADE</span>
             <span></span>
             <span id = "SearchBarSetting" >검색어</span>
             <span>|</span>
