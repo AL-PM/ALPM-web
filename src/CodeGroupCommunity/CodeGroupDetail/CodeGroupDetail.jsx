fa5a739d8ee04ccbc30d9e50bcf1dcf93925eaab
qudcks8084 <qudcks8084@naver.com>
2024-04-11 16:36:56 +0900

    [MOD] CODE_GROUP 코드 그룹 내부 코드들 출력 컴포넌트 기초 구성

--- src/CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail.jsx
+++ src/CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail.jsx
@@ -1,10 +1,70 @@
 import React from "react";
+import {useLocation} from "react-router-dom"
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
+import CodeSearchResult from "../../Etc/CodeSearchResult/CodeSearchResult.jsx";
 
-function CodeGroupDetail({codeGroupId}){
+function CodeGroupDetail(){
+    const {state} = useLocation();
+    console.log(state);
+    let exampleData = [
+        {
+            "id": 124, 
+            "name": "TestGroup",
+            "referencedCount": 3,
+            "verified": true,
+            "visible": true, 
+            "language": "JAVA",
+            "owner":  [
+                {"id": 2, 
+                 "name": "Park"
+                }
+            ], 
+            "algorithms": [
+                {
+                    "id": 1,
+                    "name": "퀵 정렬",
+                    "referencedCount": 2,
+                    "verified": true,
+                    "language": "Python",
+                    "owner" : "Byeongchan"
+                },
+                {
+                    "id": 2,
+                    "name": "버블 정렬",
+                    "referencedCount": 2,
+                    "verified": true,
+                    "language": "Python",
+                    "owner" : "Rangjin"
+                },
+                {
+                    "id": 3,
+                    "name": "힙 정렬",
+                    "referencedCount": 4,
+                    "verified": true,
+                    "language": "Python",
+                    "owner" : "HyeonWoo"
+                },
+                {
+                    "id": 4,
+                    "name": "버블 정렬",
+                    "referencedCount": 4,
+                    "verified": null,
+                    "language": "Python",
+                    "owner" : "MoonKee"
+                }
+            ] 
+        }
+    ];
+    let algorithms = [];
+
+    exampleData.forEach(group => {
+        algorithms = algorithms.concat(group.algorithms);
+    });
+    
     return(
         <div>
             <MainMenuBar page={"CodeGroup"} />
+            <CodeSearchResult searchData={algorithms}/>
         </div>
     )
 }
