b52d08172b8af84108b8c2f905fd637ae5b57cc9
qudcks8084 <qudcks8084@naver.com>
2024-04-11 17:49:24 +0900

    [MOD] CODE 코드 검색 결과 출력시 해당 코드의 상세 정보 페이지로 자동으로 전환 기능 추가

--- src/Etc/CodeSearchResult/CodeSearchResult.jsx
+++ src/Etc/CodeSearchResult/CodeSearchResult.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import './CodeSearchResult.css';
+import {useNavigate} from 'react-router-dom';
 
 function CodeSearchResult({searchData, bodyHeight}){
+    const navigator = useNavigate();
     function verifiedInvertor(Data){
         if(Data === null)
             return "ALL";
@@ -13,7 +15,7 @@ function CodeSearchResult({searchData, bodyHeight}){
 return(
     <div id="CodeSearchBody" style={{height:bodyHeight}}>
         {searchData.map((Data) =>
-            <div id="CodeSearchResult" key={Data.id}>
+            <div id="CodeSearchResult" key={Data.id} onClick={()=>navigator('/code/Detail', {state : Data.id})} >
                 <span>학습 언어</span>
                 <span>|</span>
                 <span>{Data.language}</span>
