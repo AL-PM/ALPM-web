4978f453cc119d3d2ff708cabb562516918ed217
qudcks8084 <qudcks8084@naver.com>
2024-04-11 17:07:06 +0900

    [MOD] ETC 코드그룹 상세 페이지 관련 상단 정보 바 수정

--- src/Etc/CodeGroupDetailInfo/CodeGroupDetailInfo.jsx
+++ src/Etc/CodeGroupDetailInfo/CodeGroupDetailInfo.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import './CodeGroupDetailInfo.css';
 
-function CodeGroupDetailInfo(){
+function CodeGroupDetailInfo({language, verified, owner, name, numOfAlgorithm}){
+    function verifiedInvertor(verified){
+        if(verified === null)
+            return "ALL";
+        if(verified === true)
+            return "REFERENCE";
+        if(verified === false)
+            return "USER_MADE";
+    }
     return(
-        <div>
-
-        </div>
+        <div id="CodeGroupDetailInfo">
+                <span>그룹 요약</span>
+                <span>|</span>
+                <span>{language}</span>
+                <span>{verifiedInvertor(verified)}</span>
+                <span></span>
+                <span>작성자</span>
+                <span>|</span>
+                <span>{owner}</span>
+                <span></span>
+                <span>그룹 이름</span>
+                <span>|</span>
+                <span>{name}</span>
+                <span></span>
+                <span>코드 개수</span>
+                <span>|</span>
+                <span>{numOfAlgorithm} 개</span>
+                
+            </div>
     )
 
 }
