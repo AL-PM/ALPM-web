ce842333451d9c6236b4cc13c7f09572609f777b
qudcks8084 <qudcks8084@naver.com>
2024-04-11 16:35:39 +0900

    [ADD] ETC 코드그룹상세페이지 상단 정보바 컴포넌트 추가

---
+++ src/Etc/CodeGroupDetailInfo/CodeGroupDetailInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './CodeGroupDetailInfo.css';
+
+function CodeGroupDetailInfo(){
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+
+export default CodeGroupDetailInfo;
