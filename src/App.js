7ade39ccae23055e9b87868ba20fd340eed5c33f
qudcks8084 <qudcks8084@naver.com>
2024-04-11 17:43:34 +0900

    [MOD] CODE 코드 상세 페이지 추가 및 App.js Route 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import StudyHome from "./Study/StudyHome/StudyHome";
 import MyProfile from "./MyPage/MyProfile/MyProfile";
 import CodeGroupHome from "./CodeGroupCommunity/CodeGroupHome/CodeGroupHome";
 import CodeGroupDetail from "./CodeGroupCommunity/CodeGroupDetail/CodeGroupDetail";
+import CodeDetail from "./CodeCommunity/CodeDetail/CodeDetail";
 
 
 function App(){
@@ -13,7 +14,8 @@ function App(){
             <Routes>
                 <Route path="/" element={<StudyHome />} />
                 <Route path="/mypage/myprofile" element={<MyProfile />} />
-                <Route path="/code" element={<CodeHome />} />
+                <Route path="/code/" element={<CodeHome />} />
+                <Route path="/code/Detail" element={<CodeDetail />} />
                 <Route path="/codegroup/" element={<CodeGroupHome />} />
                 <Route path="/codegroup/Detail" element={<CodeGroupDetail />} />
             </Routes>
---
+++ src/CodeCommunity/CodeDetail/CodeDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {useLocation} from "react-router-dom"
+import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
+
+function CodeDetail(){
+    return(
+        <div>
+            <MainMenuBar page={"Code"} />
+        </div>
+    )
+
+}
+
+export default CodeDetail;
