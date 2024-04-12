f1a67e944295bd41c2dd528cafa2431746296608
qudcks8084 <qudcks8084@naver.com>
2024-04-12 21:35:25 +0900

    [MOD] CODE 코드 상세 페이지 정보 요악 디자인 변경

--- src/Etc/CodeDetailInfo/CodeDetailInfo.jsx
+++ src/Etc/CodeDetailInfo/CodeDetailInfo.jsx
@@ -11,27 +11,23 @@ function CodeDetailInfo({verified, language, owner, name}){
             return "USER_MADE";
     }
     return(
-        <div>
-            <div id="CodeDetailInfoContainer" style={{marginTop:"7.5vh"}}></div>
             <div id="CodeDetailInfo">
-                <span>학습 언어</span>
+                <span style={{fontWeight:"bold"}}>학습 언어</span>
                 <span>|</span>
                 <span>{language}</span>
                 <span></span>
-                <span>코드 유형</span>
+                <span style={{fontWeight:"bold"}}>코드 유형</span>
                 <span>|</span>
                 <span>{verifiedInvertor(verified)}</span>
                 <span></span>
-                <span>작성자</span>
+                <span style={{fontWeight:"bold"}}>작성자</span>
                 <span>|</span>
                 <span>{owner}</span>
                 <span></span>
-                <span>제목</span>
+                <span style={{fontWeight:"bold"}}>제목</span>
                 <span>|</span>
                 <span>{name}</span>
             </div>
-            <div id="CodeDetailInfoContainer"></div>
-        </div>
         
     )
 }
