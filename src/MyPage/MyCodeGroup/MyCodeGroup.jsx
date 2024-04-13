3436b6443e03cffa2acbaf3abae959860e6c2ef7
qudcks8084 <qudcks8084@naver.com>
2024-04-13 16:16:34 +0900

    [ADD] MY_PAGE 마이페이지 내 코드그룹 컴포넌트 추가

---
+++ src/MyPage/MyCodeGroup/MyCodeGroup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './MyCodeGroup.css';
+
+function MyCodeGroup(){
+    return(
+        <div id="MyCodeGroup">
+
+        </div>
+    )
+}
+export default MyCodeGroup;
