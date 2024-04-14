96b74dfa99c52e987ce84adf6d988332dc33c572
qudcks8084 <qudcks8084@naver.com>
2024-04-14 19:47:58 +0900

    [MOD] MY_PAGE 새로운 코드 업로드 사이트 컴포넌트 추가 구현 및 리펙토링

--- src/MyPage/NewCodeUpload/NewCodeUpload.jsx
+++ src/MyPage/NewCodeUpload/NewCodeUpload.jsx
@@ -3,14 +3,20 @@ import './NewCodeUpload.css';
 import MainMenuBar from '../../Etc/MainMenuBar/MainMenuBar';
 import MyPageMenuBar from '../MyPageMenuBar/MyPageMenuBar';
 
-function CodeDetailTitle({Title}){
+function CodeUploadTitle({Title}){
     return(
-        <div id="CodeDetailTitle">
+        <div id="CodeUploadTitle">
             <span>|</span>
             <span>{Title}</span>
             <span>|</span>
         </div>
     )
+}
+
+function CodeUploadwBtn(){
+  return(
+      <button id="CodeUploadwBtn">새로운 코드 업로드하기</button>
+  )
 }
 
 function NewCodeUpload() {
@@ -32,11 +38,11 @@ function NewCodeUpload() {
   const descriptionKeyDown = (e) => {
     if (e.keyCode === 13) { // 엔터 키
       e.preventDefault();
-      setDescription(code + '\n');
+      setDescription(description + '\n');
     } else if (e.keyCode === 9) { // 탭 키
       e.preventDefault();
-      setDescription(code + '\t');
-    }
+      setDescription(description + '\t');
+    } 
   };
 
   return (
@@ -44,38 +50,38 @@ function NewCodeUpload() {
         <MainMenuBar page={"MyPage"} />
         <MyPageMenuBar MyPage={"5"}/>
         <div id="CodeDetailBody">
-            <CodeDetailTitle Title={"코드 언어"}/>
+            <CodeUploadTitle Title={"코드 언어"}/>
             <div id="UploadSettingInfo">
                 <span onClick={()=>setlanguage("Python")}  style={{fontWeight:language==="Python" ? "bold" : 'normal' , color : language ==="Python" ? "#EF4949" : "black"}}>Python</span>
                 <span onClick={()=>setlanguage("Java")}  style={{fontWeight:language==="Java" ? "bold" : 'normal' , color : language ==="Java" ? "#EF4949" : "black"}} >Java</span>
                 <span onClick={()=>setlanguage("C++")}  style={{fontWeight:language==="C++" ? "bold" : 'normal' , color : language ==="C++" ? "#EF4949" : "black"}} >C++</span>
             </div> 
-            <CodeDetailTitle Title={"코드 이름"}/>
-            <input id='UploadContent'
+            <CodeUploadTitle Title={"코드 이름"}/>
+            <input id='UploadContent' style={{fontSize:"medium"}}
                 value={codeName}
                 onChange={(e) => setCodeName(e.target.value)}
                 placeholder='여기에 업로드할 코드의 이름을 입력해주세요'
             />
-            <CodeDetailTitle Title={"코드 원문"}/>
-            <textarea id='UploadContent'
+            <CodeUploadTitle Title={"코드 원문"}/>
+            <textarea id='UploadContent' style={{fontSize:"large"}}
                 value={code}
                 onChange={(e) => setCode(e.target.value)}
                 onKeyDown={codeKeyDown}
-                rows={10}
-                cols={50}
+                rows={20}
+                cols={100}
                 placeholder='여기에 업로드할 코드를 입력해주세요'
             />
-            <CodeDetailTitle Title={"코드에 대한 설명"}/>
-            <textarea id='UploadContent'
+            <CodeUploadTitle Title={"코드에 대한 설명"}/>
+            <textarea id='UploadContent' style={{fontSize:"large"}}
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
                 onKeyDown={descriptionKeyDown}
-                rows={5}
-                cols={50}
+                rows={10}
+                cols={100}
                 placeholder='여기에 업로드할 코드에 대한 설명을 입력해주세요'
             />
         </div>
-      
+        <CodeUploadwBtn />
     </div>
   );
 }
