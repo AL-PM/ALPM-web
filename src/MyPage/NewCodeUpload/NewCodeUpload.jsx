7108cf84121bbead2d33c69419ba34491ab33334
qudcks8084 <qudcks8084@naver.com>
2024-04-14 18:20:09 +0900

    [MOD] MY_PAGE 새로운 코드 업로드 관련 컴포넌트 언어 및 이름 입력창 추가

--- src/MyPage/NewCodeUpload/NewCodeUpload.jsx
+++ src/MyPage/NewCodeUpload/NewCodeUpload.jsx
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
+import './NewCodeUpload.css';
+
+function CodeDetailTitle({Title}){
+    return(
+        <div id="CodeDetailTitle">
+            <span>|</span>
+            <span>{Title}</span>
+            <span>|</span>
+        </div>
+    )
+}
 
 function NewCodeUpload() {
   const [code, setCode] = useState("");
+  const [description, setDescription] = useState("");
+  const [language, setlanguage] = useState("Java");
+  const [codeName, setCodeName] = useState("");
 
-  const handleKeyDown = (e) => {
+  const codeKeyDown = (e) => {
     if (e.keyCode === 13) { // 엔터 키
       e.preventDefault();
       setCode(code + '\n');
@@ -13,15 +27,51 @@ function NewCodeUpload() {
     }
   };
 
+  const descriptionKeyDown = (e) => {
+    if (e.keyCode === 13) { // 엔터 키
+      e.preventDefault();
+      setDescription(code + '\n');
+    } else if (e.keyCode === 9) { // 탭 키
+      e.preventDefault();
+      setDescription(code + '\t');
+    }
+  };
+
   return (
     <div>
-      <textarea
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        onKeyDown={handleKeyDown}
-        rows={10}
-        cols={50}
-      />
+        <div id="CodeDetailBody">
+            <CodeDetailTitle Title={"코드 언어"}/>
+            <div id="UploadSettingInfo">
+                <span onClick={()=>setlanguage("Python")}  style={{fontWeight:language==="Python" ? "bold" : 'normal'}} >Python</span>
+                <span onClick={()=>setlanguage("Java")}  style={{fontWeight:language==="Java" ? "bold" : 'normal'}} >Java</span>
+                <span onClick={()=>setlanguage("C++")}  style={{fontWeight:language==="C++" ? "bold" : 'normal'}} >C++</span>
+            </div> 
+            <CodeDetailTitle Title={"코드 이름"}/>
+            <input id='UploadContent'
+                value={codeName}
+                onChange={(e) => setCodeName(e.target.value)}
+                placeholder='여기에 업로드할 코드의 이름을 입력해주세요'
+            />
+            <CodeDetailTitle Title={"코드 원문"}/>
+            <textarea id='UploadContent'
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                onKeyDown={codeKeyDown}
+                rows={10}
+                cols={50}
+                placeholder='여기에 업로드할 코드를 입력해주세요'
+            />
+            <CodeDetailTitle Title={"코드에 대한 설명"}/>
+            <textarea id='UploadContent'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                onKeyDown={descriptionKeyDown}
+                rows={5}
+                cols={50}
+                placeholder='여기에 업로드할 코드에 대한 설명을 입력해주세요'
+            />
+        </div>
+      
     </div>
   );
 }
