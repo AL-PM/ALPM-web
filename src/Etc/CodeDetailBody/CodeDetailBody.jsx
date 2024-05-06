1b4d9e6222ba6259b3440fc1688cb26dd593bd4d
qudcks8084 <qudcks8084@naver.com>
2024-05-07 01:25:33 +0900

    [MOD] ETC 코드 및 설명 출력 TextArea관련 내용 수정

--- src/Etc/CodeDetailBody/CodeDetailBody.jsx
+++ src/Etc/CodeDetailBody/CodeDetailBody.jsx
@@ -12,18 +12,38 @@ function CodeDetailTitle({Title}){
 }
 
 function CodeDetailBody({content, description, owner}){
+    let finalCode = "";
+
+    const data = content.split("\n");
+    for(let i = 0 ; i < data.length ; i++){
+        data[i] = data[i].split("//");
+    }
+    for(let i = 0 ; i < data.length ; i++){
+        if(data[i].length === 2){
+            finalCode = finalCode + data[i][0] + "\n";
+        }else{
+            finalCode = finalCode + data[i] + "\n";
+        }
+    }
+
+    // TextArea row 설정을 위해 코드 전체의 줄 수 계산
+    // Font-size medium 기준 +3 해야함
+    function countNumberOfCode(code) {
+        return(code.split('\n').length);
+    }
+    
     return(
         <div id="CodeDetailBody">
             <CodeDetailTitle Title={"코드 원문"}/>
-            <textarea id='CodeContent' style={{fontSize:"large"}}
-                value={content}
-                rows={20}
+            <textarea readOnly id='CodeContent'
+                value={finalCode}
+                rows={countNumberOfCode(finalCode)}
                 cols={100}
             />
             <CodeDetailTitle Title={"코드에 대한 설명"}/>
-            <textarea id='CodeContent' style={{fontSize:"large"}}
+            <textarea readOnly id='CodeDescriptionContent' 
                 value={description}
-                rows={10}
+                rows={countNumberOfCode(description)}
                 cols={100}
             />
             <CodeDetailTitle Title={"제작자"}/>
