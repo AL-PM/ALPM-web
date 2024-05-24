2530f004b30755a52e8ecdd4539c9c2d6b1df819
qudcks8084 <qudcks8084@naver.com>
2024-05-25 02:38:04 +0900

    [MOD] STUDY_HOME 줄별 순서 맞추기 코드 전처리 부분 추가

--- src/Study/StudyLineOrdering/StudyLineOrdering.jsx
+++ src/Study/StudyLineOrdering/StudyLineOrdering.jsx
@@ -1,47 +1,134 @@
 import React from "react";
 import './StudyLineOrdering.css';
 
-function StudyLineOrdering({language, method, level, codegroup, problem}){
+function StudyLineOrdering(){
     const code = {
         "level" : 1,
-        "text" : '#include <bits/stdc++.h>\n#define ll long long\nusing namespace std;\nll N, M;\nll arr[100005];\nvector<ll> SegTree;\nll Query(ll n, ll l, ll r, ll st, ll ed) {\n\t@if (l > $ed$ || r < $st$) // 탐색 범위를 벗어나는 경우\n\t\treturn $0$;\n\tif (l >= $st$ && r <= $ed$) // 목표 범위에 속하는 경우\n\t\treturn $SegTree[n]$;\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\treturn Query($n * 2$, $l$, $mid$, $st$, $ed$) + Query($n * 2 + 1$, $mid + 1$, $r$, $st$, $ed$); // 왼쪽 구간과 오른쪽 구간을 탐색\n}\n\nll Update(ll n, ll l, ll r, ll pos, ll x) {\n\tif (l > $pos$ || r < $pos$) // 탐색범위를 벗어나면\n\t\treturn $SegTree[n]$; // 해당 세그먼트 트리의 값을 반환\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $x$;\n\t\treturn $SegTree[n]$; // 새로 입력받은 값을 반환\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Update($n * 2$, $l$, $mid$, $pos$, $x$) + Update($n * 2 + 1$, $mid + 1$, $r$, $pos$, $x$); // 왼쪽 구간과 오른쪽 구간을 탐색\n\treturn $SegTree[n]$;\n}\n\nll Init(ll n, ll l, ll r) {\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $arr[l]$; // arr[l] 값이 상위노드로 넘어감\n\t\treturn $SegTree[n]$;\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Init($n * 2$, $l$, $mid$) + Init($n * 2 + 1$, $mid + 1$, $r$); // 왼쪽 구간과 오른쪽 구간으로 분할\n\treturn $SegTree[n]$;\n}\n\nint main() {\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n\tcin >> N >> M;\n\tfor (int i = 1; i <= N; i++)\n\t\tcin >> arr[i]; // 원소 입력 받음\n\n\tSegTree.resize(4 * N); // 세그먼트 트리 크기가 4*N 인 벡터 생성\n\tInit(1, 1, N); // 세그먼트 트리 초기화\n\n\tfor (int i = 0; i < M; i++) {\n\t\tll x, a, b;\n\t\tcin >> x >> a >> b;\n\t\tif (x) // x가 1이면\n\t\t\tcout << Query(1, 1, N, a, b) << "\\n"; // a번째부터 b번째까지의 합 출력\n\t\telse // x가 0이면\n\t\t\tUpdate(1, 1, N, a, b); // a번째 원소를 b로 변경\n\t}\n\n\treturn 0;\n}'
+        "text" : '#include <bits/stdc++.h>\n#define ll long long\nusing namespace std;\nll N, M;\nll arr[100005];\nvector<ll> SegTree;\nll Query(ll n, ll l, ll r, ll st, ll ed) {\n\tif (l > ed || r < st) // 탐색 범위를 벗어나는 경우\n\t\treturn 0;\n\tif (l >= st && r <= ed) // 목표 범위에 속하는 경우\n\t\treturn SegTree[n];\n\tll mid = (l + r) / 2; // 중심값 선언\n\treturn Query(n * 2, l, mid, st, ed) + Query(n * 2 + 1, mid + 1, r, st, ed); // 왼쪽 구간과 오른쪽 구간을 탐색\n}\n\nll Update(ll n, ll l, ll r, ll pos, ll x) {\n\tif (l > pos || r < pos) // 탐색범위를 벗어나면\n\t\treturn SegTree[n]; // 해당 세그먼트 트리의 값을 반환\n\tif (l == r) { // leaf node에 도달하면\n\t\tSegTree[n] = x;\n\t\treturn SegTree[n]; // 새로 입력받은 값을 반환\n\t}\n\tll mid = (l + r) / 2; // 중심값 선언\n\tSegTree[n] = Update(n * 2, l, mid, pos, x) + Update(n * 2 + 1, mid + 1, r, pos, x); // 왼쪽 구간과 오른쪽 구간을 탐색\n\treturn SegTree[n];\n}\n\nll Init(ll n, ll l, ll r) {\n\tif (l == r) { // leaf node에 도달하면\n\t\tSegTree[n] = arr[l]; // arr[l] 값이 상위노드로 넘어감\n\t\treturn SegTree[n];\n\t}\n\tll mid = (l + r) / 2; // 중심값 선언\n\tSegTree[n] = Init(n * 2, l, mid) + Init(n * 2 + 1, mid + 1, r); // 왼쪽 구간과 오른쪽 구간으로 분할\n\treturn SegTree[n];\n}\n\nint main() {\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n\tcin >> N >> M;\n\tfor (int i = 1; i <= N; i++)\n\t\tcin >> arr[i]; // 원소 입력 받음\n\n\tSegTree.resize(4 * N); // 세그먼트 트리 크기가 4*N 인 벡터 생성\n\tInit(1, 1, N); // 세그먼트 트리 초기화\n\n\tfor (int i = 0; i < M; i++) {\n\t\tll x, a, b;\n\t\tcin >> x >> a >> b;\n\t\tif (x) // x가 1이면\n\t\t\tcout << Query(1, 1, N, a, b) << "\\n"; // a번째부터 b번째까지의 합 출력\n\t\telse // x가 0이면\n\t\t\tUpdate(1, 1, N, a, b); // a번째 원소를 b로 변경\n\t}\n\n\treturn 0;\n}'
     };
 
-    let finalCode = "";
+    function preprocessCode(code) {
+        let lines = code.split("\n");
+        let processedCode = [];
 
-    // 코드별 설명 분리
-    const data = code.text.split("\n");
-    for(let i = 0 ; i < data.length ; i++){
-        data[i] = data[i].split("//");
+        let currentSection = 0;
+        let numOfOpenBracket = 0;
+        let inFunction = false;
+        let lineNumberInFunction = 0;
+
+        for (let i = 0; i < lines.length; i++) {
+            let line = lines[i];
+            let trimmedLine = line.trim();
+
+            if (trimmedLine !== "") {
+                if (trimmedLine.match("{")) {
+                    if (numOfOpenBracket === 0) {
+                        processedCode.push({
+                            data: line.split("//")[0],
+                            num: 0,
+                            codeSection: 0,
+                        });
+                        currentSection += 1;
+
+                    } else {
+                        processedCode.push({
+                            data: line.split("//")[0],
+                            num: lineNumberInFunction,
+                            codeSection: currentSection,
+                        });
+                    }
+                    numOfOpenBracket += 1;
+                    inFunction = true;
+                } else if (trimmedLine === "}") {
+                    numOfOpenBracket -= 1;
+                    processedCode.push({
+                        data: line.split("//")[0],
+                        num: lineNumberInFunction,
+                        codeSection: numOfOpenBracket === 0 ? 0 : currentSection,
+                    });
+                    if (numOfOpenBracket === 0) {
+                        inFunction = false;
+                        lineNumberInFunction = 0;
+                    } else {
+                        lineNumberInFunction += 1;
+                    }
+                } else {
+                    processedCode.push({
+                        data: line.split("//")[0],
+                        num: inFunction ? lineNumberInFunction : 0,
+                        codeSection: numOfOpenBracket === 0 ? 0 : currentSection,
+                    });
+                    if (inFunction) {
+                        lineNumberInFunction += 1;
+                    }
+                }
+            }
+        }
+
+        console.log(processedCode);
+        return processedCode;
     }
-    for(let i = 0 ; i < data.length ; i++){
-        if(data[i].length === 2){
-            finalCode = finalCode + data[i][0] + "\n";
-        }else{
-            finalCode = finalCode + data[i] + "\n";
+
+    function devideFn(processedCode) {
+        let finalCode = [];
+        let tmpCode = [];
+        let nowCodeSection = 1;
+    
+        processedCode.forEach((element, index) => {
+            if (element.codeSection === nowCodeSection) {
+                tmpCode.push(element);
+            } else if (element.codeSection > nowCodeSection) {
+                finalCode.push(tmpCode);
+                tmpCode = [];
+                nowCodeSection = element.codeSection;
+                tmpCode.push(element);
+            }
+    
+            // Ensure the last section is added
+            if (index === processedCode.length - 1) {
+                finalCode.push(tmpCode);
+            }
+        });
+    
+        return finalCode;
+    }
+
+    function shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
     }
 
-    // TextArea row 설정을 위해 코드 전체의 줄 수 계산
-    // Font-size medium 기준 +3 해야함
-    function countNumberOfCode(code) {
-        return(code.split('\n').length + 3);
+    function randomFn(finalCode) {
+        // Make a deep copy of the finalCode array
+        let randomCode = JSON.parse(JSON.stringify(finalCode));
+        
+        // Shuffle each section within the randomCode array
+        randomCode.forEach(section => {
+            shuffle(section);
+        });
+        return randomCode;
     }
 
-    return(
+    let processedData = preprocessCode(code.text);
+    let finalCode = devideFn(processedData);
+    let randomfinalCode = randomFn(finalCode);
+
+    console.log(finalCode, randomfinalCode);
+
+    return (
         <div id="StudyLineOrdering">
-            <ul id="StudyLineOrderingInfo">
-            <li>학습 언어 : {language}</li>
-            <li>학습 방법 : {method}</li>
-            <li>학습 레벨 : {level}</li> 
-            <li>학습 그룹 ID : {codegroup}</li>
-            </ul> 
-            <textarea readOnly
-                id="StudyLineOrderingCodeArea" 
-                rows={countNumberOfCode(finalCode)}
-                cols={150}
-                defaultValue={finalCode} // defaultValue 속성 사용
-            />
+            {processedData.map((codeData, index) => 
+            <div key={index}>
+                <textarea readOnly id="StudyTrackingBackground" 
+                rows={1}
+                cols={140}
+                defaultValue={codeData.codeSection === 0 ? codeData.data : ""} 
+                tabIndex={-1}
+                />
+            </div>
+            )} 
         </div>
     )
 }
