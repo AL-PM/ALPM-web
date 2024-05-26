e43c003f826279dee89891b1e811eebbada66593
qudcks8084 <qudcks8084@naver.com>
2024-05-26 12:27:38 +0900

    [MOD] STUDY_HOME 줄별 순서 맞추기 자동 랜더링 추가

--- src/Study/StudyLineOrdering/StudyLineOrdering.jsx
+++ src/Study/StudyLineOrdering/StudyLineOrdering.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './StudyLineOrdering.css';
 
 function StudyLineOrdering() {
     const [currentPage, setCurrentPage] = useState(0);
     const [userInput, setUserInput] = useState([]);
-    const [randomfinalCode, setRandomFinalCode] = useState([]);
-    const [processedData, setProcessedData] = useState();
+    const [randomFinalCode, setRandomFinalCode] = useState([]);
+    const [processedData, setProcessedData] = useState([]);
 
     const code = {
         "level": 1,
-        "text": '#include <bits/stdc++.h>\n#define ll long long\nusing namespace std;\nll N, M;\nll arr[100005];\nvector<ll> SegTree;\nll Query(ll n, ll l, ll r, ll st, ll ed) {\n\tif (l > ed || r < st) // 탐색 범위를 벗어나는 경우\n\t\treturn 0;\n\tif (l >= st && r <= ed) // 목표 범위에 속하는 경우\n\t\treturn SegTree[n];\n\tll mid = (l + r) / 2; // 중심값 선언\n\treturn Query(n * 2, l, mid, st, ed) + Query(n * 2 + 1, mid + 1, r, st, ed); // 왼쪽 구간과 오른쪽 구간을 탐색\n}\n\nll Update(ll n, ll l, ll r, ll pos, ll x) {\n\tif (l > pos || r < pos) // 탐색범위를 벗어나면\n\t\treturn SegTree[n]; // 해당 세그먼트 트리의 값을 반환\n\tif (l == r) { // leaf node에 도달하면\n\t\tSegTree[n] = x;\n\t\treturn SegTree[n]; // 새로 입력받은 값을 반환\n\t}\n\tll mid = (l + r) / 2; // 중심값 선언\n\tSegTree[n] = Update(n * 2, l, mid, pos, x) + Update(n * 2 + 1, mid + 1, r, pos, x); // 왼쪽 구간과 오른쪽 구간을 탐색\n\treturn SegTree[n];\n}\n\nll Init(ll n, ll l, ll r) {\n\tif (l == r) { // leaf node에 도달하면\n\t\tSegTree[n] = arr[l]; // arr[l] 값이 상위노드로 넘어감\n\t\treturn SegTree[n];\n\t}\n\tll mid = (l + r) / 2; // 중심값 선언\n\tSegTree[n] = Init(n * 2, l, mid) + Init(n * 2 + 1, mid + 1, r); // 왼쪽 구간과 오른쪽 구간으로 분할\n\treturn SegTree[n];\n}\n\nint main() {\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n\tcin >> N >> M;\n\tfor (int i = 1; i <= N; i++)\n\t\tcin >> arr[i]; // 원소 입력 받음\n\n\tSegTree.resize(4 * N); // 세그먼트 트리 크기가 4*N 인 벡터 생성\n\tInit(1, 1, N); // 세그먼트 트리 초기화\n\n\tfor (int i = 0; i < M; i++) {\n\t\tll x, a, b;\n\t\tcin >> x >> a >> b;\n\t\tif (x) // x가 1이면\n\t\t\tcout << Query(1, 1, N, a, b) << "\\n"; // a번째부터 b번째까지의 합 출력\n\t\telse // x가 0이면\n\t\t\tUpdate(1, 1, N, a, b); // a번째 원소를 b로 변경\n\t}\n\n\treturn 0;\n}'
+        "text" : '#include <bits/stdc++.h>\n#define ll long long\nusing namespace std;\nll N, M;\nll arr[100005];\nvector<ll> SegTree;\nll Query(ll n, ll l, ll r, ll st, ll ed) {\n\tif (l > $ed$ || r < $st$) // 탐색 범위를 벗어나는 경우\n\t\treturn $0$;\n\tif (l >= $st$ && r <= $ed$) // 목표 범위에 속하는 경우\n\t\treturn $SegTree[n]$;\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\treturn Query($n * 2$, $l$, $mid$, $st$, $ed$) + Query($n * 2 + 1$, $mid + 1$, $r$, $st$, $ed$); // 왼쪽 구간과 오른쪽 구간을 탐색\n}\n\nll Update(ll n, ll l, ll r, ll pos, ll x) {\n\tif (l > $pos$ || r < $pos$) // 탐색범위를 벗어나면\n\t\treturn $SegTree[n]$; // 해당 세그먼트 트리의 값을 반환\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $x$;\n\t\treturn $SegTree[n]$; // 새로 입력받은 값을 반환\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Update($n * 2$, $l$, $mid$, $pos$, $x$) + Update($n * 2 + 1$, $mid + 1$, $r$, $pos$, $x$); // 왼쪽 구간과 오른쪽 구간을 탐색\n\treturn $SegTree[n]$;\n}\n\nll Init(ll n, ll l, ll r) {\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $arr[l]$; // arr[l] 값이 상위노드로 넘어감\n\t\treturn $SegTree[n]$;\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Init($n * 2$, $l$, $mid$) + Init($n * 2 + 1$, $mid + 1$, $r$); // 왼쪽 구간과 오른쪽 구간으로 분할\n\treturn $SegTree[n]$;\n}\n\nint main() {\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n\tcin >> N >> M;\n\tfor (int i = 1; i <= N; i++)\n\t\tcin >> arr[i]; // 원소 입력 받음\n\n\tSegTree.resize(4 * N); // 세그먼트 트리 크기가 4*N 인 벡터 생성\n\tInit(1, 1, N); // 세그먼트 트리 초기화\n\n\tfor (int i = 0; i < M; i++) {\n\t\tll x, a, b;\n\t\tcin >> x >> a >> b;\n\t\tif (x) // x가 1이면\n\t\t\tcout << Query(1, 1, N, a, b) << "\\n"; // a번째부터 b번째까지의 합 출력\n\t\telse // x가 0이면\n\t\t\tUpdate(1, 1, N, a, b); // a번째 원소를 b로 변경\n\t}\n\n\treturn 0;\n}'
     };
 
     function preprocessCode(code) {
@@ -44,7 +44,7 @@ function StudyLineOrdering() {
                             num: lineNumberInFunction,
                             codeSection: currentSection,
                         });
-                        lineNumberInFunction += 1;  // Increase lineNumberInFunction here
+                        lineNumberInFunction += 1;
                     }
                 } else if (trimmedLine === "}") {
                     numOfOpenBracket -= 1;
@@ -57,7 +57,7 @@ function StudyLineOrdering() {
                         inFunction = false;
                         lineNumberInFunction = 0;
                     } else {
-                        lineNumberInFunction += 1;  // Increase lineNumberInFunction here
+                        lineNumberInFunction += 1;
                     }
                 } else {
                     processedCode.push({
@@ -126,7 +126,11 @@ function StudyLineOrdering() {
         setRandomFinalCode(randomfinalCode);
     }, [code.text]);
 
-    console.log(randomfinalCode, userInput);
+    useEffect(() => {
+        // This effect runs whenever userInput is updated to trigger re-render
+    }, [userInput]);
+
+    console.log(randomFinalCode, userInput);
 
     function exampleFn(eachBlock) {
         setUserInput(prevUserInput => {
@@ -141,31 +145,39 @@ function StudyLineOrdering() {
             return updatedRandomFinalCode;
         });
     }
-    
+
     return (
         <div>
             <div id="StudyLineOrdering">
                 {processedData && processedData.map((codeData, index) =>
                     <div key={index}>
-                        {codeData.codeSection === 0 ? <textarea readOnly id="StudyTrackingBackground"
-                            rows={1}
-                            cols={140}
-                            defaultValue={codeData.data}
-                            tabIndex={-1}
-                        />
-                            :
-                            <textarea readOnly id="StudyTrackingBackground"
+                        {codeData.codeSection === 0 ? (
+                            <textarea
+                                readOnly
+                                id="StudyTrackingBackground"
                                 rows={1}
                                 cols={140}
-                                defaultValue={"[__" + codeData.codeSection + "," + (codeData.num + 1) + "__]"}
+                                value={codeData.data}
                                 tabIndex={-1}
-                            />}
+                            />
+                        ) : (
+                            <textarea
+                                readOnly
+                                id="StudyTrackingBackground"
+                                rows={1}
+                                cols={140}
+                                value={userInput[codeData.codeSection - 1][codeData.num] ? userInput[codeData.codeSection - 1][codeData.num].data : `[__${codeData.codeSection},${codeData.num + 1}__]`}
+                                tabIndex={-1}
+                            />
+                        )}
                     </div>
                 )}
                 <div id="exampleBox">
-                    <p style={{ fontFamily: 'SUITE-Regular' }}>{"[ " + (currentPage + 1)  +"번 페이지 보기 ]"} </p>
+                    <p style={{ fontFamily: 'SUITE-Regular' }}>
+                        {"[ " + (currentPage + 1) + "번 페이지 보기 ]"}
+                    </p>
                     <div id="LineOrderingexampleList">
-                        {randomfinalCode[currentPage] && randomfinalCode[currentPage].map((eachBlock) =>
+                        {randomFinalCode[currentPage] && randomFinalCode[currentPage].map((eachBlock) =>
                             <p
                                 key={eachBlock.num}
                                 id="LineOredringExampleList"
@@ -176,9 +188,13 @@ function StudyLineOrdering() {
                         )}
                     </div>
                     <div>
-                        {randomfinalCode.map((_, index) => (
-                            <button id="codeSectionPagination" key={index} onClick={() => setCurrentPage(index)}>
-                                {"_"+(index + 1)}
+                        {randomFinalCode.map((_, index) => (
+                            <button
+                                id="codeSectionPagination"
+                                key={index}
+                                onClick={() => setCurrentPage(index)}
+                            >
+                                {"_" + (index + 1)}
                             </button>
                         ))}
                     </div>
@@ -189,4 +205,3 @@ function StudyLineOrdering() {
 }
 
 export default StudyLineOrdering;
-
