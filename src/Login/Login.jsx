c540553d495e0b14b2bce467b916984310380014
qudcks8084 <qudcks8084@naver.com>
2024-05-28 18:56:46 +0900

    [MOD] LOGIN 로그인 관련 CSS 수정

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import GoogleLoginBtn from './GoogleLoginBtn';
+import './Login.css';
 
 function Login() {
+  /*
   const handleGoogleLogin = () => {
     window.location.href = "http://alpm.duckdns.org/oauth2/authorization/google";
   };
+  */
 
   return (
-    <div>
-      <p>On Boarding Page Design</p>
-      <button onClick={handleGoogleLogin}>Google Login</button>
+    <div id='Login'>
+      <span id='LoginMainLogo'>AL-PM</span>
+      <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
       <GoogleLoginBtn />
     </div>
   );
