05955731ab048091bfa18f7aedb6d8189573c982
qudcks8084 <qudcks8084@naver.com>
2024-05-29 12:10:04 +0900

    [MOD] LOGIN axios 호출 파라미터 수정

--- src/Login/GoogleLoginNew.jsx
+++ src/Login/GoogleLoginNew.jsx
@@ -8,31 +8,31 @@ function GoogleLoginNew() {
     flow: 'auth-code',
     onSuccess: (res) => {
       console.log(res);
-        axios.get(`https://alpm.duckdns.org/oauth2/code/google`,{
-          code : res.code,
-        })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+      axios.get(`https://alpm.duckdns.org/oauth2/code/google`, {
+        params: {
+          code: res.code,
+        },
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
     },
-    onError: (error) =>{ console.log(error);}
-});
+    onError: (error) => { console.log(error); }
+  });
 
-  
-    return (
-      <div id='Login'>
-        <span id='LoginMainLogo'>AL-PM</span>
-        <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
-        <div id='GoogleBtnContainer'>
-          <button onClick={() => signIn()}>Sign in with Google 🚀</button>
-          <GoogleLoginBtn />
-        </div>
-        
+  return (
+    <div id='Login'>
+      <span id='LoginMainLogo'>AL-PM</span>
+      <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
+      <div id='GoogleBtnContainer'>
+        <button onClick={() => signIn()}>Sign in with Google 🚀</button>
+        <GoogleLoginBtn />
       </div>
-    );
+    </div>
+  );
 }
 
 export default GoogleLoginNew;
