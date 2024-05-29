dbd7280b5e1e475d1c0e6836250acd880e319615
qudcks8084 <qudcks8084@naver.com>
2024-05-29 13:27:05 +0900

    [TEST] LOGIN

--- src/Login/GoogleLoginNew.jsx
+++ src/Login/GoogleLoginNew.jsx
@@ -3,29 +3,19 @@ import GoogleLoginBtn from './GoogleLoginBtn';
 import './GoogleLoginNew.css';
 
 function GoogleLoginNew() {
-  const signIn = useGoogleLogin({
-    flow: 'auth-code',
-    onSuccess: async (res) => {
-      try {
-        console.log(res);
-        const response = await fetch(`https://alpm.duckdns.org/oauth2/code/google?code=${res.code}`, {
-          method: 'GET',
-        });
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    onError: (error) => { console.log(error); }
-  });
+
+  const ClientID = "974624999104-as2donm7225f9ofp74er00qvfob57hgm.apps.googleusercontent.com"
+  const handleLogin = () => {
+    // 구글 로그인 화면으로 이동시키기
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://alpm.pages.dev/&client_id=974624999104-as2donm7225f9ofp74er00qvfob57hgm.apps.googleusercontent.com`;
+  };
 
   return (
     <div id='Login'>
       <span id='LoginMainLogo'>AL-PM</span>
       <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
       <div id='GoogleBtnContainer'>
-        <button onClick={() => signIn()}>Sign in with Google 🚀</button>
+        <button onClick={() => handleLogin()}>Sign in with Google 🚀</button>
         <GoogleLoginBtn />
       </div>
     </div>
