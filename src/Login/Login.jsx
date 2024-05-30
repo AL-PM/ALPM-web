fbb2995ecaa044fe981ecb16e8e759c7704cace6
qudcks8084 <qudcks8084@naver.com>
2024-05-30 14:42:16 +0900

    [MOD] LOGIN 관련 내용 수정

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import GoogleLoginNew from './GoogleLoginNew';
+import React, { useEffect } from 'react';
+import './Login.css';
 
 function Login() {
+
+  const handleLogin = () => {
+    // Redirect the user to Google login screen
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://alpm.pages.dev/&client_id=974624999104-as2donm7225f9ofp74er00qvfob57hgm.apps.googleusercontent.com`;
+  };
+
+  useEffect(() => {
+    // Check if the URL contains the 'code' parameter after redirection
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    if (code) {
+      // Log the 'code' value to the console
+      console.log(code);
+
+      fetch(`https://alpm.duckdns.org:8080/oauth2/code/google?code=${code}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+
+      // You can perform further actions here, such as exchanging the code for an access token
+    }
+  }, []); // This effect will run only once after component mount
+
   return (
-    <div>
-      <GoogleOAuthProvider clientId='974624999104-as2donm7225f9ofp74er00qvfob57hgm.apps.googleusercontent.com' >
-        <GoogleLoginNew />
-      </GoogleOAuthProvider>
+    <div id='Login'>
+      <span id='LoginMainLogo'>AL-PM</span>
+      <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
+      <div id='GoogleBtnContainer'>
+        <button onClick={handleLogin}>Sign in with Google 🚀</button>
+      </div>
     </div>
   );
 }
