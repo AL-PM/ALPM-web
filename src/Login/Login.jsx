9cb7f7a55c2422d26a95726386d08c2595800927
qudcks8084 <qudcks8084@naver.com>
2024-05-31 02:13:28 +0900

    [MOD] LOGIN TEST LocalStorage 저장문 수정

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -27,10 +27,10 @@ function Login() {
       })
         .then(response => {
 
-          console.log(response);
+          console.log(response.data);
 
           // Save access_token and refresh_token to localStorage
-          localStorage.setItem('uid', response.data.id);
+          localStorage.setItem('uid', response.data.user.id);
           localStorage.setItem('access_token', response.data.access_token);
           localStorage.setItem('refresh_token', response.data.refresh_token);
 
