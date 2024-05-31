c7c0dbf01febe8ca0a8c60feb15994b65a0ee8b9
qudcks8084 <qudcks8084@naver.com>
2024-06-01 02:58:12 +0900

    [MOD] MY_PAGE 내 코드 보드 조회 API 콜 추가

--- src/MyPage/MyCodeBoard/MyCodeBoard.jsx
+++ src/MyPage/MyCodeBoard/MyCodeBoard.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import CodeSearchResult from "../../Etc/CodeSearchResult/CodeSearchResult";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar";
 import MyPageMenuBar from "../MyPageMenuBar/MyPageMenuBar";
 
 function MyCodeBoard(){
+
+    const [searchResult, setSearchResult] = useState();
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const access_token = localStorage.getItem("access_token");
+                const uid = localStorage.getItem("uid");
+
+                const response = await axios.get(`https://alpm.duckdns.org:8080/algorithm/user/${uid}`, {
+                    withCredentials: true,
+                    headers: {
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                });
+
+                setSearchResult(response.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserData();
+
+    }, []);
+
+    if (!searchResult) {
+        return (
+            <div id="MyCodeGroup">
+                <MainMenuBar page={"MyPage"} />
+                <MyPageMenuBar MyPage={"2"}/>
+            </div>
+        );
+    }
+
+    console.log(searchResult);
+
     const searchData = [
         {
             "id": 1,
