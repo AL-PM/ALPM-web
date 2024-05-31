29f95c8f6a8da9ce6fbf16cd9084dc8ba8af2bf3
qudcks8084 <qudcks8084@naver.com>
2024-06-01 03:22:03 +0900

    [MOD] CODE_GROUP 초기 데이터 호출  조회 API 콜 추가

--- src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
+++ src/CodeGroupCommunity/CodeGroupHome/CodeGroupHome.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import "./CodeGroupHome.css";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
 import CommunitySearchBar from "../../Etc/CommunitySearchBar/CommunitySearchBar.jsx";
@@ -9,77 +10,48 @@ function CodeGrupHome(){
     const [language, setlanguage] = useState("PYTHON");
     const [reference, setreference] = useState("ALL");
     const [searchkeyword, setkeword] = useState("");
-    const searchData = [
-        {
-            "id": 1,
-            "name": "다양한 정렬 알고리즘",
-            "referencedCount": 2,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "Byeongchan"
-        },
-        {
-            "id": 2,
-            "name": "스택 관련 자료 구조",
-            "referencedCount": 2,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "Rangjin"
-        },
-        {
-            "id": 3,
-            "name": "Knapsack",
-            "referencedCount": 4,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "HyeonWoo"
-        },
-        {
-            "id": 4,
-            "name": "n-queen problem",
-            "referencedCount": 4,
-            "verified": null,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 5,
-            "name": "Linked-List",
-            "referencedCount": 4,
-            "verified": null,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 6,
-            "name": "Binary Search",
-            "referencedCount": 4,
-            "verified": false,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 7,
-            "name": "Tree Search",
-            "referencedCount": 4,
-            "verified": false,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-    ]
+    const [searchResult, setSearchResult] = useState();
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const access_token = localStorage.getItem("access_token");
+
+                const response = await axios.get(`https://alpm.duckdns.org:8080/codeGroup`, {
+                    withCredentials: true,
+                    headers: {
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                });
+
+                setSearchResult(response.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserData();
+
+    }, []);
+
+    if (!searchResult) {
+        return (
+            <div id="MyCodeGroup">
+                <MainMenuBar page={"CodeGroup"} />
+                <CommunitySearchBar secondTag={"제작자 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
+            </div>
+        );
+    }
+
+    console.log(searchResult);
+
 
     return(
         <div id = "CodeGroupHome">
             <MainMenuBar page={"CodeGroup"} />
             <CommunitySearchBar secondTag={"제작자 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
-            <CodeGroupSearchResult searchData={searchData} bodyHeight={"60vh"}/>
+            <CodeGroupSearchResult searchData={searchResult.content} bodyHeight={"60vh"}/>
         </div>
        
     )
