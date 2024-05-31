34fd3d3b6c313a5faf46d345085d2e9146071b42
qudcks8084 <qudcks8084@naver.com>
2024-06-01 03:26:27 +0900

    [MOD] CODE 초기 데이터 호출  조회 API 콜 추가

--- src/CodeCommunity/CodeHome/CodeHome.jsx
+++ src/CodeCommunity/CodeHome/CodeHome.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import "./CodeHome.css";
 import {useNavigate} from "react-router-dom";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar.jsx"
@@ -18,46 +19,48 @@ function CodeCommunity(){
     const [language, setlanguage] = useState("PYTHON");
     const [reference, setreference] = useState("ALL");
     const [searchkeyword, setkeword] = useState("");
-    const searchData = [
-        {
-            "id": 1,
-            "name": "퀵 정렬",
-            "referencedCount": 2,
-            "verified": true,
-            "language": "Python",
-            "owner" : "Byeongchan"
-        },
-        {
-            "id": 2,
-            "name": "버블 정렬",
-            "referencedCount": 2,
-            "verified": true,
-            "language": "Python",
-            "owner" : "Rangjin"
-        },
-        {
-            "id": 3,
-            "name": "힙 정렬",
-            "referencedCount": 4,
-            "verified": true,
-            "language": "Python",
-            "owner" : "HyeonWoo"
-        },
-        {
-            "id": 4,
-            "name": "버블 정렬",
-            "referencedCount": 4,
-            "verified": null,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        
-    ]
+    const [searchResult, setSearchResult] = useState();
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const access_token = localStorage.getItem("access_token");
+
+                const response = await axios.get(`https://alpm.duckdns.org:8080/algorithm`, {
+                    withCredentials: true,
+                    headers: {
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                });
+
+                setSearchResult(response.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserData();
+
+    }, []);
+
+    if (!searchResult) {
+        return (
+            <div id="MyCodeGroup">
+                <MainMenuBar page={"Code"} />
+                <CommunitySearchBar secondTag={"코드 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
+            </div>
+        );
+    }
+
+    console.log(searchResult);
+
+    
     return(
         <div id = "Codehome">
             <MainMenuBar page={"Code"} />
             <CommunitySearchBar secondTag={"코드 유형"} language = {language} setlanguage={setlanguage} reference= {reference} setreference = {setreference} setkeword={setkeword} searchkeyword={searchkeyword} />
-            <CodeSearchResult searchData={searchData} bodyHeight={"55vh"} siteTag={"Code"}/>
+            <CodeSearchResult searchData={searchResult.content} bodyHeight={"55vh"} siteTag={"Code"}/>
             <CodeHomeUploadButton />
         </div>
        
