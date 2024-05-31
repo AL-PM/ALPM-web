78b6ac59c2e1d51942ee9333d151eec41a467eb1
qudcks8084 <qudcks8084@naver.com>
2024-06-01 01:56:58 +0900

    [MOD] MY_PAGE 내 코드그룹 조회 더미 데이터 삭제 및 데이터 연결

--- src/MyPage/MyCodeGroup/MyCodeGroup.jsx
+++ src/MyPage/MyCodeGroup/MyCodeGroup.jsx
@@ -49,76 +49,12 @@ function MyCodeGroup(){
 
     console.log(searchResult);
 
-    const searchData = [
-        {
-            "id": 1,
-            "name": "다양한 정렬 알고리즘",
-            "referencedCount": 2,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "Byeongchan"
-        },
-        {
-            "id": 2,
-            "name": "스택 관련 자료 구조",
-            "referencedCount": 2,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "Rangjin"
-        },
-        {
-            "id": 3,
-            "name": "Knapsack",
-            "referencedCount": 4,
-            "verified": true,
-            "visible": true,
-            "language": "Python",
-            "owner" : "HyeonWoo"
-        },
-        {
-            "id": 4,
-            "name": "n-queen problem",
-            "referencedCount": 4,
-            "verified": null,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 5,
-            "name": "Linked-List",
-            "referencedCount": 4,
-            "verified": null,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 6,
-            "name": "Binary Search",
-            "referencedCount": 4,
-            "verified": false,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 7,
-            "name": "Tree Search",
-            "referencedCount": 4,
-            "verified": false,
-            "visible": true,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-    ]
+
     return(
         <div id="MyCodeGroup">
             <MainMenuBar page={"MyPage"} />
             <MyPageMenuBar MyPage={"3"}/>
-            <CodeGroupSearchResult searchData={searchData} bodyHeight={"60vh"}/>
+            <CodeGroupSearchResult searchData={searchResult.content} bodyHeight={"60vh"}/>
             <MyCodeGroupNew />
         </div>
     )
