912407132ccce9b3b336851f7e92e8cf87d2b140
qudcks8084 <qudcks8084@naver.com>
2024-06-01 03:13:28 +0900

    [MOD] MY_PAGE 내가 작성한 코드 조회 API 콜 추가

--- src/MyPage/MyUploadCode/MyUploadCode.jsx
+++ src/MyPage/MyUploadCode/MyUploadCode.jsx
@@ -1,72 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import CodeSearchResult from "../../Etc/CodeSearchResult/CodeSearchResult";
 import MainMenuBar from "../../Etc/MainMenuBar/MainMenuBar";
 import MyPageMenuBar from "../MyPageMenuBar/MyPageMenuBar";
 
 function MyUploadCode(){
-    const searchData = [
-        {
-            "id": 1,
-            "name": "퀵 정렬",
-            "referencedCount": 2,
-            "verified": true,
-            "language": "Python",
-            "owner" : "Byeongchan"
-        },
-        {
-            "id": 2,
-            "name": "버블 정렬",
-            "referencedCount": 2,
-            "verified": true,
-            "language": "Python",
-            "owner" : "Rangjin"
-        },
-        {
-            "id": 3,
-            "name": "힙 정렬",
-            "referencedCount": 4,
-            "verified": true,
-            "language": "Python",
-            "owner" : "HyeonWoo"
-        },
-        {
-            "id": 4,
-            "name": "버블 정렬",
-            "referencedCount": 4,
-            "verified": null,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 4,
-            "name": "버블 정렬",
-            "referencedCount": 4,
-            "verified": null,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 4,
-            "name": "버블 정렬",
-            "referencedCount": 4,
-            "verified": null,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-        {
-            "id": 4,
-            "name": "버블 정렬",
-            "referencedCount": 4,
-            "verified": null,
-            "language": "Python",
-            "owner" : "MoonKee"
-        },
-    ]
+
+    const [searchResult, setSearchResult] = useState();
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const access_token = localStorage.getItem("access_token");
+                const uid = localStorage.getItem("uid");
+
+                const response = await axios.get(`https://alpm.duckdns.org:8080/algorithm/owner/${uid}`, {
+                    withCredentials: true,
+                    headers: {
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                });
+
+                setSearchResult(response.data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchUserData();
+
+    }, []);
+
+    if (!searchResult) {
+        return (
+            <div id="MyCodeGroup">
+                <MainMenuBar page={"MyPage"} />
+                <MyPageMenuBar MyPage={"4"}/>
+            </div>
+        );
+    }
+
+    console.log(searchResult);
+
     return(
         <div id="MyUploadCode">
             <MainMenuBar page={"MyPage"} />
             <MyPageMenuBar MyPage={"4"}/>
-            <CodeSearchResult searchData={searchData} bodyHeight={"66vh"}/>
+            <CodeSearchResult searchData={searchResult.content} bodyHeight={"66vh"}/>
         </div>
     )
 }
