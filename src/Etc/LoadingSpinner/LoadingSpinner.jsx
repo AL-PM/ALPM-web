7d831efb9ee9e7427531d5681e168231c470eaa0
qudcks8084 <qudcks8084@naver.com>
2024-06-02 00:48:07 +0900

    [MOD] SPINNER 수정

--- src/Etc/LoadingSpinner/LoadingSpinner.jsx
+++ src/Etc/LoadingSpinner/LoadingSpinner.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { HashLoader} from 'react-spinners';
+import { HashLoader } from 'react-spinners';
 import './LoadingSpinner.css';
 
 function LoadingSpinner({color, comment}){
     return(
         <div id="Spinner">
-            <HashLoader color={color} />
+            <HashLoader 
+                color={color} 
+                speedMultiplier={0.8}
+            />
             {comment ? <p>{comment}</p> : null}
         </div>
     )
