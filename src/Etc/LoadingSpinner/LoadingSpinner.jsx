c25a3cfe90d837dd3a4ff9680d6073e9ffcf9c10
qudcks8084 <qudcks8084@naver.com>
2024-06-01 22:02:18 +0900

    [MOD] ETC 로딩 스피너 수정

--- src/Etc/LoadingSpinner/LoadingSpinner.jsx
+++ src/Etc/LoadingSpinner/LoadingSpinner.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import SpinnerIMG from './img/LoadingSpinner1.gif'
+import SpinnerIMG from './img/LoadingSpinner1.gif';
 
-function LoadingSpinner(){
-    return(
-        <div>
-            <img src={SpinnerIMG} alt="로딩 이미지"></img>
+function LoadingSpinner() {
+    const spinnerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+    };
+
+    return (
+        <div style={spinnerStyle}>
+            <img src={SpinnerIMG} alt="로딩 이미지" />
         </div>
-    )
+    );
 }
 
 export default LoadingSpinner;
