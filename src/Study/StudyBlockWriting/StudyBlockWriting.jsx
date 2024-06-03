aa1b8511e6873f11ae554ad1f932e10a8cda7d5f
qudcks8084 <qudcks8084@naver.com>
2024-06-03 18:24:05 +0900

    [MOD] 빈칸 채우기 API 연결

--- src/Study/StudyBlockWriting/StudyBlockWriting.jsx
+++ src/Study/StudyBlockWriting/StudyBlockWriting.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import './StudyBlockWriting.css';
 
-function StudyBlockWriting() {
+function StudyBlockWriting({problemCode}) {
     const [codeData, setCodeData] = useState(null);
     const [userInput, setUserInput] = useState([]);
     const [finalCode, setFinalCode] = useState("");
 
     useEffect(() => {
         const code = {
-            "level": 1,
-            "text" : '#include <bits/stdc++.h>\n#define ll long long\nusing namespace std;\nll N, M;\nll arr[100005];\nvector<ll> SegTree;\nll Query(ll n, ll l, ll r, ll st, ll ed) {\n\tif (l > $ed$ || r < $st$) // 탐색 범위를 벗어나는 경우\n\t\treturn $0$;\n\tif (l >= $st$ && r <= $ed$) // 목표 범위에 속하는 경우\n\t\treturn $SegTree[n]$;\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\treturn Query($n * 2$, $l$, $mid$, $st$, $ed$) + Query($n * 2 + 1$, $mid + 1$, $r$, $st$, $ed$); // 왼쪽 구간과 오른쪽 구간을 탐색\n}\n\nll Update(ll n, ll l, ll r, ll pos, ll x) {\n\tif (l > $pos$ || r < $pos$) // 탐색범위를 벗어나면\n\t\treturn $SegTree[n]$; // 해당 세그먼트 트리의 값을 반환\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $x$;\n\t\treturn $SegTree[n]$; // 새로 입력받은 값을 반환\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Update($n * 2$, $l$, $mid$, $pos$, $x$) + Update($n * 2 + 1$, $mid + 1$, $r$, $pos$, $x$); // 왼쪽 구간과 오른쪽 구간을 탐색\n\treturn $SegTree[n]$;\n}\n\nll Init(ll n, ll l, ll r) {\n\tif (l == $r$) { // leaf node에 도달하면\n\t\tSegTree[n] = $arr[l]$; // arr[l] 값이 상위노드로 넘어감\n\t\treturn $SegTree[n]$;\n\t}\n\tll mid = $(l + r) / 2$; // 중심값 선언\n\tSegTree[n] = Init($n * 2$, $l$, $mid$) + Init($n * 2 + 1$, $mid + 1$, $r$); // 왼쪽 구간과 오른쪽 구간으로 분할\n\treturn $SegTree[n]$;\n}\n\nint main() {\n\tios::sync_with_stdio(0);\n\tcin.tie(0);\n\n\tcin >> N >> M;\n\tfor (int i = 1; i <= N; i++)\n\t\tcin >> arr[i]; // 원소 입력 받음\n\n\tSegTree.resize(4 * N); // 세그먼트 트리 크기가 4*N 인 벡터 생성\n\tInit(1, 1, N); // 세그먼트 트리 초기화\n\n\tfor (int i = 0; i < M; i++) {\n\t\tll x, a, b;\n\t\tcin >> x >> a >> b;\n\t\tif (x) // x가 1이면\n\t\t\tcout << Query(1, 1, N, a, b) << "\\n"; // a번째부터 b번째까지의 합 출력\n\t\telse // x가 0이면\n\t\t\tUpdate(1, 1, N, a, b); // a번째 원소를 b로 변경\n\t}\n\n\treturn 0;\n}'
+            text : problemCode.content,
+            language : problemCode.language
         };
 
         // UserInput 초기화
@@ -35,7 +35,7 @@ function StudyBlockWriting() {
                 result.splice(numOfBlank);
                 return result;
             }
-
+        
             let finalCode = [];
             let trimmedCode = "";
             let processedCode = [];
@@ -43,19 +43,22 @@ function StudyBlockWriting() {
             let sortedblockData = [];
             let sortedlineupblockData = [];
             let lines = code.split("\n");
-
+        
             function extractCode(line) {
                 let codeWithoutComment = line.split("//")[0];
                 let tmpCode = codeWithoutComment.replace(/\s+$/, '');
+                if (code.language === "PYTHON") {
+                    tmpCode = codeWithoutComment.split("#")[0];
+                }
                 trimmedCode += tmpCode + "\n";
             }
-
+        
             for (let i = 0; i < lines.length; i++) {
                 extractCode(lines[i]);
             }
-
+        
             let dollarSplitted = trimmedCode.split("$");
-
+        
             for (let i = 0; i < dollarSplitted.length; i++) {
                 let blank = 0;
                 if (i % 2 === 1) {
@@ -72,9 +75,9 @@ function StudyBlockWriting() {
                     blank: blank,
                 });
             }
-
+        
             blockData = getRandomNumbers(blockData, 3);
-
+        
             blockData.forEach((element) => {
                 sortedblockData.push({
                     data: element.data,
@@ -82,10 +85,10 @@ function StudyBlockWriting() {
                     blank: element.blank,
                 });
             });
-
+        
             let lineUp = 0;
             sortedblockData.sort((a, b) => a.num - b.num);
-
+        
             sortedblockData.forEach((element) => {
                 sortedlineupblockData.push({
                     data: element.data,
@@ -95,16 +98,17 @@ function StudyBlockWriting() {
                 });
                 lineUp++;
             });
-
+        
             finalCode = [processedCode, blockData, sortedlineupblockData];
-
+        
             return finalCode;
         }
+        
 
         const preprocessedCode = preprocessCode(code.text);
         setCodeData(preprocessedCode);
 
-    }, []);
+    }, [problemCode]);
 
     useEffect(() => {
         if (codeData) {
