63312ec4ef10ce79cee7a2ba593aae0f4ee4eb8d
qudcks8084 <qudcks8084@naver.com>
2024-06-03 19:59:47 +0900

    [MOD] MY_PAGE 잔디 추가

--- src/MyPage/MyProfile/ContributionGraph.jsx
+++ src/MyPage/MyProfile/ContributionGraph.jsx
@@ -7,27 +7,27 @@ function ContributionGraph({ historyList }) {
     const filteredHistoryList = historyList.filter(item => new Date(item.date) <= currentDate);
 
     // Prepare data for the contribution graph
-    const contributions = Array(7).fill().map(() => Array(Math.ceil(filteredHistoryList.length / 7)).fill(0));
+    const contributions = Array(Math.ceil(filteredHistoryList.length / 7)).fill().map(() => Array(7).fill(0));
 
     filteredHistoryList.forEach((item, index) => {
         const week = Math.floor(index / 7);
         const day = index % 7;
-        contributions[day][week] = item.size;
+        contributions[week][day] = item.size;
     });
 
     return (
         <div className="contribution-graph">
-            {contributions.map((day, dayIndex) => (
-                <div key={dayIndex} className="day">
-                    {day.map((value, weekIndex) => {
+            {contributions.map((week, weekIndex) => (
+                <div key={weekIndex} className="week">
+                    {week.map((day, dayIndex) => {
                         const date = new Date(currentDate.getFullYear(), 0, 1);
                         date.setDate(date.getDate() + (weekIndex * 7) + dayIndex);
                         const dateString = date.toISOString().split('T')[0];
                         return (
                             <div 
-                                key={weekIndex} 
-                                className={`day-${value}`} 
-                                title={`${dateString} | 학습 포인트: ${value}`}
+                                key={dayIndex} 
+                                className={`day day-${day}`} 
+                                title={`${dateString} | 학습 포인트: ${day}`}
                             >
                             </div>
                         );
