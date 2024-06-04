9659597a3073020af2c9c3b63e5bcd5067e0febd
qudcks8084 <qudcks8084@naver.com>
2024-06-04 22:13:36 +0900

    [MOD] ETC 커뮤니티 검색 바 관련 CSS 수정

--- src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
+++ src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
@@ -21,18 +21,75 @@ function CommunitySearchBar({ secondTag, language, setLanguage, reference, setRe
         <div id="CommunitySearchBar">
             <span id="SearchBarSetting">학습 언어</span>
             <span>|</span>
-            <button onClick={() => setLanguage("PYTHON")} style={{ fontWeight: language === "PYTHON" ? "bold" : "normal", color: language === "PYTHON" ? colorInvertor(secondTag) : "black" }}>PYTHON </button>
-            <button onClick={() => setLanguage("JAVA")} style={{ fontWeight: language === "JAVA" ? "bold" : "normal", color: language === "JAVA" ? colorInvertor(secondTag) : "black" }}>JAVA</button>
-            <button onClick={() => setLanguage("C")} style={{ fontWeight: language === "C" ? "bold" : "normal", color: language === "C" ? colorInvertor(secondTag) : "black" }}>C++</button>
+            <button 
+                className={`menu-button ${language === "PYTHON" ? "active" : ""}`} 
+                style={{ color: language === "PYTHON" ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setLanguage("PYTHON")}
+                data-tag={secondTag}
+            >
+                PYTHON
+            </button>
+            <span>|</span>
+            <button 
+                className={`menu-button ${language === "JAVA" ? "active" : ""}`} 
+                style={{ color: language === "JAVA" ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setLanguage("JAVA")}
+                data-tag={secondTag}
+            >
+                JAVA
+            </button>
+            <span>|</span>
+            <button 
+                className={`menu-button ${language === "C" ? "active" : ""}`} 
+                style={{ color: language === "C" ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setLanguage("C")}
+                data-tag={secondTag}
+            >
+                C++
+            </button>
             <span id="SearchBarSetting">{secondTag}</span>
             <span>|</span>
-            <button onClick={() => setReference(null)} style={{ fontWeight: reference === null ? "bold" : "normal", color: reference === null ? colorInvertor(secondTag) : "black" }}>ALL</button>
-            <button onClick={() => setReference(true)} style={{ fontWeight: reference === true ? "bold" : "normal", color: reference === true ? colorInvertor(secondTag) : "black" }}>REFERENCE</button>
-            <button onClick={() => setReference(false)} style={{ fontWeight: reference === false ? "bold" : "normal", color: reference === false ? colorInvertor(secondTag) : "black" }}>USER_MADE</button>
+            <button 
+                className={`menu-button ${reference === null ? "active" : ""}`} 
+                style={{ color: reference === null ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setReference(null)}
+                data-tag={secondTag}
+            >
+                ALL
+            </button>
+            <span>|</span>
+            <button 
+                className={`menu-button ${reference === true ? "active" : ""}`} 
+                style={{ color: reference === true ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setReference(true)}
+                data-tag={secondTag}
+            >
+                REFERENCE
+            </button>
+            <span>|</span>
+            <button 
+                className={`menu-button ${reference === false ? "active" : ""}`} 
+                style={{ color: reference === false ? colorInvertor(secondTag) : "black" }}
+                onClick={() => setReference(false)}
+                data-tag={secondTag}
+            >
+                USER_MADE
+            </button>
             <span id="SearchBarSetting">검색어</span>
             <span>|</span>
-            <input id="SearchKeywordInput" value={searchKeyword} onChange={(event) => setKeyword(event.target.value)} type="text" name="CodeHomeSearchKeyword" placeholder="키워드를 입력해주세요"></input>
-            {searchIsOn ? <button id="DataLoadbutton" onClick={searchResetBtn}> 전체 코드보기 </button> : <button id="DataLoadbutton" onClick={searchButton}> 검색하기 </button>}
+            <input 
+                id="SearchKeywordInput" 
+                value={searchKeyword} 
+                onChange={(event) => setKeyword(event.target.value)} 
+                type="text" 
+                name="CodeHomeSearchKeyword" 
+                placeholder="키워드를 입력해주세요"
+            />
+            {searchIsOn ? (
+                <button id="DataLoadbutton" onClick={searchResetBtn}> 전체 코드보기 </button>
+            ) : (
+                <button id="DataLoadbutton" onClick={searchButton}> 검색하기 </button>
+            )}
         </div>
     );
 }
