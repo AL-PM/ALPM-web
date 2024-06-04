d1ca2e15957ea6186376e63158c68b7ae2934a60
qudcks8084 <qudcks8084@naver.com>
2024-06-04 18:06:04 +0900

    [MOD] MY_PAGE 내 코드 보드 조회 페이징 추가

--- src/MyPage/MyCodeBoard/MyCodeBoard.jsx
+++ src/MyPage/MyCodeBoard/MyCodeBoard.jsx
@@ -67,7 +67,7 @@ function MyCodeBoard(){
             <MyPageMenuBar MyPage={"2"}/>
             <CodeSearchResult searchData={searchResult.content} bodyHeight={"55vh"} siteTag={"MyPage"}/>
             <div id="pagination">
-                <button id='paginationBtn' onClick={handlePreviousPage} disabled={currentPage === 0}>Previous</button>
+                <button id='paginationBtn' onClick={handlePreviousPage} disabled={currentPage === 0}>Prev</button>
                 <span id='paginationTxt'> {currentPage + 1} / {searchResult.total_pages}</span>
                 <button id='paginationBtn' onClick={handleNextPage} disabled={currentPage >= searchResult.total_pages - 1}>Next</button>
             </div>
