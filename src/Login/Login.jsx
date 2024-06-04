33a3afb2604649b0fc3b7e091251e826d93605bf
qudcks8084 <qudcks8084@naver.com>
2024-06-05 02:01:03 +0900

    [MOD] LOGIN 비동기 로그인을 동기로 수정 및 스피너 추가

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import googleLogo from './img/Google-Logo.png';
+import { HashLoader } from 'react-spinners';
 import './Login.css';
 
 function Login() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = () => {
+    setLoading(true); // Set loading to true when the login button is clicked
     // Redirect the user to Google login screen
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?scope=https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://alpm.pages.dev/&client_id=974624999104-as2donm7225f9ofp74er00qvfob57hgm.apps.googleusercontent.com`;
   };
@@ -18,7 +21,6 @@ function Login() {
     const code = urlParams.get('code');
 
     if (code) {
-
       // Use Axios to send the request
       axios.get(`https://alpm.duckdns.org:8080/oauth2/code/google`, {
         params: { code },
@@ -26,7 +28,6 @@ function Login() {
         withCredentials: true, // Include credentials if needed
       })
         .then(response => {
-
           // Save access_token and refresh_token to localStorage
           localStorage.setItem('uid', response.data.user.id);
           localStorage.setItem('access_token', response.data.access_token);
@@ -38,22 +39,32 @@ function Login() {
           navigate('/study');
         })
         .catch(error => {
-
-          alert("로그인에 실패하였습니다. 다시 시도해주세요.")
+          alert("로그인에 실패하였습니다. 다시 시도해주세요.");
           // Redirect to the login page on failure
           window.location.href = 'https://alpm.pages.dev/';
+        })
+        .finally(() => {
+          setLoading(false); // Set loading to false after the response
         });
+    } else {
+      setLoading(false); // Set loading to false if there is no code parameter
     }
-  }, [navigate]); // This effect will run only once after component mount
+  }, [navigate]);
 
   return (
     <div id='Login'>
       <span id='LoginMainLogo'>AL-PM</span>
       <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
       <div id='GoogleBtnContainer'>
-        <button id='GoogleBtn' onClick={handleLogin}>
-          <img id='loginGoogleLogo' src={googleLogo} alt="googleLogo" />
-          <span> 구글로 로그인하기 </span>
+        <button id='GoogleBtn' onClick={handleLogin} disabled={loading}>
+          {loading ? (
+            <HashLoader color={"red"} speedMultiplier={0.8} />
+          ) : (
+            <>
+              <img id='loginGoogleLogo' src={googleLogo} alt="googleLogo" />
+              <span> 구글로 로그인하기 </span>
+            </>
+          )}
         </button>
       </div>
     </div>
