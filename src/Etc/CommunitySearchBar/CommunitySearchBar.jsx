1cbdefafab215692ae20f0899f0caa70311a5a38
qudcks8084 <qudcks8084@naver.com>
2024-06-04 19:02:14 +0900

    [MOD] ETC 커뮤니티 검색바 버튼 수정

--- src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
+++ src/Etc/CommunitySearchBar/CommunitySearchBar.jsx
@@ -21,14 +21,14 @@ function CommunitySearchBar({ secondTag, language, setLanguage, reference, setRe
         <div id="CommunitySearchBar">
             <span id="SearchBarSetting">학습 언어</span>
             <span>|</span>
-            <span onClick={() => setLanguage("PYTHON")} style={{ fontWeight: language === "PYTHON" ? "bold" : "normal", color: language === "PYTHON" ? colorInvertor(secondTag) : "black" }}>PYTHON </span>
-            <span onClick={() => setLanguage("JAVA")} style={{ fontWeight: language === "JAVA" ? "bold" : "normal", color: language === "JAVA" ? colorInvertor(secondTag) : "black" }}>JAVA</span>
-            <span onClick={() => setLanguage("C")} style={{ fontWeight: language === "C" ? "bold" : "normal", color: language === "C" ? colorInvertor(secondTag) : "black" }}>C++</span>
+            <button onClick={() => setLanguage("PYTHON")} style={{ fontWeight: language === "PYTHON" ? "bold" : "normal", color: language === "PYTHON" ? colorInvertor(secondTag) : "black" }}>PYTHON </button>
+            <button onClick={() => setLanguage("JAVA")} style={{ fontWeight: language === "JAVA" ? "bold" : "normal", color: language === "JAVA" ? colorInvertor(secondTag) : "black" }}>JAVA</button>
+            <button onClick={() => setLanguage("C")} style={{ fontWeight: language === "C" ? "bold" : "normal", color: language === "C" ? colorInvertor(secondTag) : "black" }}>C++</button>
             <span id="SearchBarSetting">{secondTag}</span>
             <span>|</span>
-            <span onClick={() => setReference(null)} style={{ fontWeight: reference === null ? "bold" : "normal", color: reference === null ? colorInvertor(secondTag) : "black" }}>ALL</span>
-            <span onClick={() => setReference(true)} style={{ fontWeight: reference === true ? "bold" : "normal", color: reference === true ? colorInvertor(secondTag) : "black" }}>REFERENCE</span>
-            <span onClick={() => setReference(false)} style={{ fontWeight: reference === false ? "bold" : "normal", color: reference === false ? colorInvertor(secondTag) : "black" }}>USER_MADE</span>
+            <button onClick={() => setReference(null)} style={{ fontWeight: reference === null ? "bold" : "normal", color: reference === null ? colorInvertor(secondTag) : "black" }}>ALL</button>
+            <button onClick={() => setReference(true)} style={{ fontWeight: reference === true ? "bold" : "normal", color: reference === true ? colorInvertor(secondTag) : "black" }}>REFERENCE</button>
+            <button onClick={() => setReference(false)} style={{ fontWeight: reference === false ? "bold" : "normal", color: reference === false ? colorInvertor(secondTag) : "black" }}>USER_MADE</button>
             <span id="SearchBarSetting">검색어</span>
             <span>|</span>
             <input id="SearchKeywordInput" value={searchKeyword} onChange={(event) => setKeyword(event.target.value)} type="text" name="CodeHomeSearchKeyword" placeholder="키워드를 입력해주세요"></input>
