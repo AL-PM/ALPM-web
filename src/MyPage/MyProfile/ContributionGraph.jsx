c49086a8d74edd6c736fdd8d22cc29fcf1d89056
qudcks8084 <qudcks8084@naver.com>
2024-06-05 15:07:44 +0900

    [MOD] MY_PAGE 잔디 관련 출력 문구 수정

--- src/MyPage/MyProfile/ContributionGraph.jsx
+++ src/MyPage/MyProfile/ContributionGraph.jsx
@@ -31,7 +31,7 @@ function ContributionGraph({ historyList }) {
                             <div 
                                 key={dayIndex} 
                                 className={`day ${isToday ? 'today' : ''} ${dayClass}`} 
-                                title={`${dateString} | 학습 포인트: ${day}`}
+                                title={`${dateString} | 학습한 문제 : ${day} 개`}
                             >
                             </div>
                         );
