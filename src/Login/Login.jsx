9af7f7e9e8baa515a6099ea578298ab7e932e850
qudcks8084 <qudcks8084@naver.com>
2024-06-05 09:30:22 +0900

    [MOD] LOGIN 관련 CSS 색 변경

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,9 +5,18 @@ import googleLogo from './img/Google-Logo.png';
 import { HashLoader } from 'react-spinners';
 import './Login.css';
 
+function Banner({ message, type }) {
+  return (
+    <div className={`banner ${type}`}>
+      {message}
+    </div>
+  );
+}
+
 function Login() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const [banner, setBanner] = useState({ show: false, message: '', type: '' });
 
   const handleLogin = () => {
     setLoading(true); // Set loading to true when the login button is clicked
@@ -33,15 +42,20 @@ function Login() {
           localStorage.setItem('access_token', response.data.access_token);
           localStorage.setItem('refresh_token', response.data.refresh_token);
 
-          alert(" 안녕하세요 " + response.data.user.name + " 사용자님! \n 정상적으로 로그인되었습니다.");
+          setBanner({ show: true, message: `안녕하세요 ${response.data.user.name} 사용자님! 정상적으로 로그인되었습니다.`, type: 'success' });
 
-          // Navigate to /study on successful login
-          navigate('/study');
+          // Navigate to /study after 3 seconds
+          setTimeout(() => {
+            navigate('/study');
+          }, 3000);
         })
         .catch(error => {
-          alert("로그인에 실패하였습니다. 다시 시도해주세요.");
-          // Redirect to the login page on failure
-          window.location.href = 'https://alpm.pages.dev/';
+          setBanner({ show: true, message: '로그인에 실패하였습니다. 다시 시도해주세요.', type: 'error' });
+
+          // Reload the page after 3 seconds
+          setTimeout(() => {
+            window.location.reload();
+          }, 3000);
         })
         .finally(() => {
           setLoading(false); // Set loading to false after the response
@@ -53,22 +67,23 @@ function Login() {
 
   return (
     <div id='Login'>
+      {banner.show && <Banner message={banner.message} type={banner.type} />}
       <span id='LoginMainLogo'>AL-PM</span>
       <span id='LoginDesctiption'>새로운 알고리즘 학습의 시작!</span>
       {loading ?
-            <div style={{display : "flex", flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
-              <HashLoader color={"#5C4EFF"} speedMultiplier={0.8} />
-              <span style={{marginTop : "30px", color:"#5C4EFF"}}>로그인 시도중</span>
-            </div>
-           : 
-          <div id='GoogleBtnContainer'>
-            <button id='GoogleBtn' onClick={handleLogin} disabled={loading}>
-              <img id='loginGoogleLogo' src={googleLogo} alt="googleLogo" />
-              <span> 구글로 로그인하기 </span>
-            </button>
-          </div>  
-          }
-     </div> 
+        <div style={{ display: "flex", flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <HashLoader color={"#5C4EFF"} speedMultiplier={0.8} />
+          <span style={{ marginTop: "30px", color: "#5C4EFF" }}>로그인 시도중</span>
+        </div>
+        :
+        <div id='GoogleBtnContainer'>
+          <button id='GoogleBtn' onClick={handleLogin} disabled={loading}>
+            <img id='loginGoogleLogo' src={googleLogo} alt="googleLogo" />
+            <span> 구글로 로그인하기 </span>
+          </button>
+        </div>
+      }
+    </div>
   );
 }
 
