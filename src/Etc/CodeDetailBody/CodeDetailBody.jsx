debfca34d6b9a0f53162bfbe037533c98ee63c7b
qudcks8084 <qudcks8084@naver.com>
2024-06-05 09:08:14 +0900

    [MOD] ETC 코드 상세 페이지에서 자동으로 $가 삭제되어서 출력되도록 수정

--- src/Etc/CodeDetailBody/CodeDetailBody.jsx
+++ src/Etc/CodeDetailBody/CodeDetailBody.jsx
@@ -2,27 +2,27 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import './CodeDetailBody.css';
 
-function CodeDetailTitle({Title}){
-    return(
+function CodeDetailTitle({ Title }) {
+    return (
         <div id="CodeDetailTitle">
             <span>|</span>
             <span>{Title}</span>
             <span>|</span>
         </div>
-    )
+    );
 }
 
-function CodeDetailBody({content, description, owner}){
+function CodeDetailBody({ content, description, owner }) {
     let finalCode = "";
 
     const data = content.split("\n");
-    for(let i = 0 ; i < data.length ; i++){
+    for (let i = 0; i < data.length; i++) {
         data[i] = data[i].split("//");
     }
-    for(let i = 0 ; i < data.length ; i++){
-        if(data[i].length === 2){
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].length === 2) {
             finalCode = finalCode + data[i][0] + "\n";
-        }else{
+        } else {
             finalCode = finalCode + data[i] + "\n";
         }
     }
@@ -30,29 +30,37 @@ function CodeDetailBody({content, description, owner}){
     // TextArea row 설정을 위해 코드 전체의 줄 수 계산
     // Font-size medium 기준 +3 해야함
     function countNumberOfCode(code) {
-        return(code.split('\n').length);
+        return code.split('\n').length;
     }
-    
-    return(
+
+    // Remove all instances of the "$" character
+    const sanitizedDescription = description.replace(/\$/g, "");
+
+    return (
         <div id="CodeDetailBody">
-            <CodeDetailTitle Title={"코드 원문"}/>
-            <textarea readOnly id='CodeContent'
+            <CodeDetailTitle Title={"코드 원문"} />
+            <textarea
+                readOnly
+                id="CodeContent"
                 value={finalCode}
                 rows={countNumberOfCode(finalCode)}
                 cols={100}
             />
-            <CodeDetailTitle Title={"코드에 대한 설명"}/>
-            <div id='CodeDescriptionContent' >
-                <ReactMarkdown>{description.replace("$","")}</ReactMarkdown>
+            <CodeDetailTitle Title={"코드에 대한 설명"} />
+            <div id="CodeDescriptionContent">
+                <ReactMarkdown>{sanitizedDescription}</ReactMarkdown>
             </div>
-            <CodeDetailTitle Title={"작성자"}/>
+            <CodeDetailTitle Title={"작성자"} />
             <div id="UserContent">
-                <img id="CodeDetailUserProfile" src={owner.profile} alt="User Profile" />
+                <img
+                    id="CodeDetailUserProfile"
+                    src={owner.profile}
+                    alt="User Profile"
+                />
                 <span>{owner.name}</span>
             </div>
-            
         </div>
-    )
+    );
 }
 
 export default CodeDetailBody;
