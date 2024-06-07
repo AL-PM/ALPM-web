019e82a5e451472225150053c08b8a486260cd2f
qudcks8084 <qudcks8084@naver.com>
2024-06-07 17:19:55 +0900

    [MOD] MY_PAGE 잔디 색상 및 로직 변경

--- src/MyPage/MyProfile/ContributionGraph.jsx
+++ src/MyPage/MyProfile/ContributionGraph.jsx
@@ -30,10 +30,12 @@ function ContributionGraph({ historyList }) {
                         // Determine the class based on item.size
                         let sizeClass;
                         if (item.size < 1) {
+                            sizeClass = 'day-0';
+                        }else if (item.size === 1){
                             sizeClass = 'day-1';
-                        } else if (item.size === 1) {
-                            sizeClass = 'day-2';
                         } else if (item.size === 2) {
+                            sizeClass = 'day-2';
+                        } else if (item.size === 3) {
                             sizeClass = 'day-3';
                         } else {
                             sizeClass = 'day-4';
